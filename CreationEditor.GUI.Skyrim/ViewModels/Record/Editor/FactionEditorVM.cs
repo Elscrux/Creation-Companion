61c407429a8163b563d839a8b6d9d7f8cfb1524b
nickp <nickposer2102@gmail.com>
2022-10-23 22:04:00 +0200

    Expanded faction editor

--- CreationEditor.GUI.Skyrim/ViewModels/Record/Editor/FactionEditorVM.cs
+++ CreationEditor.GUI.Skyrim/ViewModels/Record/Editor/FactionEditorVM.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Reactive;
 using System.Windows.Controls;
 using CreationEditor.Environment;
@@ -16,6 +14,7 @@ using Mutagen.Bethesda.Strings;
 using Noggog;
 using Noggog.WPF;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 namespace CreationEditor.GUI.Skyrim.ViewModels.Record;
 
 public class FactionEditorVM : ViewModel, IRecordEditorVM<Faction, IFactionGetter> {
@@ -23,7 +22,7 @@ public class FactionEditorVM : ViewModel, IRecordEditorVM<Faction, IFactionGette
     private readonly IEditorEnvironment _editorEnvironment;
     
     public Faction Record { get; set; } = null!;
-    public EditableFaction EditableRecord { get; set; } = null!;
+    [Reactive] public EditableFaction EditableRecord { get; set; } = null!;
 
     public ILinkCache LinkCache => _editorEnvironment.LinkCache;
 
@@ -33,12 +32,14 @@ public class FactionEditorVM : ViewModel, IRecordEditorVM<Faction, IFactionGette
     public ReactiveCommand<Unit, Unit> AddRank { get; }
     public ReactiveCommand<Unit, Unit> RemoveSelectedRank { get; }
     
-    public Relation? SelectedRelation { get; set; }
-    public ObservableCollection<object> SelectedRanks { get; set; } = new();
+    public int SelectedRelationIndex { get; set; }
+    public int SelectedRankIndex { get; set; }
     
     public IEnumerable<Type> OutfitTypes { get; } = typeof(IOutfitGetter).AsEnumerable();
     public IEnumerable<Type> FormListTypes { get; } = typeof(IFormListGetter).AsEnumerable();
     
+    public static IEnumerable<Type> RelationTypes { get; } = new[] { typeof(IFactionGetter), typeof(IRaceGetter) };
+    public static IEnumerable<CombatReaction> CombatReactions { get; } = Enum.GetValues<CombatReaction>();
     public FactionEditorVM(
         IRecordEditorController recordEditorController,
         IEditorEnvironment editorEnvironment) {
@@ -56,8 +57,9 @@ public class FactionEditorVM : ViewModel, IRecordEditorVM<Faction, IFactionGette
         });
         
         RemoveSelectedRelations = ReactiveCommand.Create(() => {
-            if (SelectedRelation == null) return;
-            EditableRecord.Relations.RemoveWhere(relation => SelectedRelation.Equals(relation));
+            if (SelectedRelationIndex < 0 || SelectedRelationIndex >= EditableRecord.Relations.Count) return;
+
+            EditableRecord.Relations.RemoveAt(SelectedRelationIndex);
         });
         
         AddRank = ReactiveCommand.Create(() => {
@@ -65,7 +67,9 @@ public class FactionEditorVM : ViewModel, IRecordEditorVM<Faction, IFactionGette
         });
         
         RemoveSelectedRank = ReactiveCommand.Create(() => {
-            EditableRecord.Ranks.Remove(SelectedRanks.Cast<Rank>());
+            if (SelectedRankIndex < 0 || SelectedRankIndex >= EditableRecord.Ranks.Count) return;
+
+            EditableRecord.Ranks.RemoveAt(SelectedRankIndex);
         });
     }
     
--- CreationEditor.Skyrim/Models/Records/EditableFaction.cs
+++ CreationEditor.Skyrim/Models/Records/EditableFaction.cs
@@ -1,82 +1,128 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Mutagen.Bethesda.Skyrim;
 using Noggog;
 namespace CreationEditor.Skyrim.Models.Records; 
 
-public sealed class EditableFaction : Faction {
+public sealed class EditableFaction : Faction, INotifyPropertyChanged {
     public new ObservableCollection<Relation> Relations { get; set; }
     public new ObservableCollection<Rank> Ranks { get; set; }
     public new CrimeValues CrimeValues { get; set; }
     public new VendorValues VendorValues { get; set; }
 
+    public override string? EditorID { get; set; }
+    
     public bool HiddenFromPc {
         get => (Flags & FactionFlag.HiddenFromPC) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.HiddenFromPC, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.HiddenFromPC, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool SpecialCombat {
         get => (Flags & FactionFlag.SpecialCombat) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.SpecialCombat, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.SpecialCombat, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool CanBeOwner {
         get => (Flags & FactionFlag.CanBeOwner) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.CanBeOwner, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.CanBeOwner, value);
+            OnPropertyChanged();
+        }
     }
 
     public string? NameStr {
         get => Name?.String;
-        set => Name = value;
+        set {
+            Name = value;
+            OnPropertyChanged();
+        }
     }
 
     public bool IgnoreAssault {
         get => (Flags & FactionFlag.IgnoreAssault) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.IgnoreAssault, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.IgnoreAssault, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool IgnoreMurder {
         get => (Flags & FactionFlag.IgnoreMurder) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.IgnoreMurder, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.IgnoreMurder, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool IgnorePickpocket {
         get => (Flags & FactionFlag.IgnorePickpocket) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.IgnorePickpocket, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.IgnorePickpocket, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool IgnoreStealing {
         get => (Flags & FactionFlag.IgnoreStealing) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.IgnoreStealing, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.IgnoreStealing, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool IgnoreTrespass {
         get => (Flags & FactionFlag.IgnoreTrespass) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.IgnoreTrespass, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.IgnoreTrespass, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool IgnoreWerewolf {
         get => (Flags & FactionFlag.IgnoreWerewolf) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.IgnoreWerewolf, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.IgnoreWerewolf, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool DoNotReportCrimesAgainstMembers {
         get => (Flags & FactionFlag.DoNotReportCrimesAgainstMembers) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.DoNotReportCrimesAgainstMembers, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.DoNotReportCrimesAgainstMembers, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool TrackCrime {
         get => (Flags & FactionFlag.TrackCrime) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.TrackCrime, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.TrackCrime, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool CrimeGoldUseDefaults {
         get => (Flags & FactionFlag.CrimeGoldUseDefaults) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.CrimeGoldUseDefaults, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.CrimeGoldUseDefaults, value);
+            OnPropertyChanged();
+        }
     }
 
     public bool Vendor {
         get => (Flags & FactionFlag.Vendor) != 0;
-        set => Flags = Flags.SetFlag(FactionFlag.Vendor, value);
+        set {
+            Flags = Flags.SetFlag(FactionFlag.Vendor, value);
+            OnPropertyChanged();
+        }
     }
     
     public EditableFaction(IFaction parent) {
@@ -86,18 +132,20 @@ public sealed class EditableFaction : Faction {
         Flags = parent.Flags;
         Relations = new ObservableCollection<Relation>(parent.Relations);
         Ranks = new ObservableCollection<Rank>(parent.Ranks);
-        CrimeValues = parent.CrimeValues ?? new CrimeValues {
-            Arrest = true,
-            AttackOnSight = true,
-            Murder = 1000,
-            Assault = 40,
-            Pickpocket = 25,
-            Trespass = 5,
-            StealMult = 0.5f,
-            Escape = 100,
-            Werewolf = 1000
-        };
+        
+        CrimeValues = parent.CrimeValues ?? GetDefaultCrimeValues();
+        JailOutfit = parent.JailOutfit;
+        SharedCrimeFactionList = parent.SharedCrimeFactionList;
+        ExteriorJailMarker = parent.ExteriorJailMarker;
+        FollowerWaitMarker = parent.FollowerWaitMarker;
+        PlayerInventoryContainer = parent.PlayerInventoryContainer;
+        StolenGoodsContainer = parent.StolenGoodsContainer;
+        
         VendorValues = parent.VendorValues ?? new VendorValues();
+        VendorLocation = parent.VendorLocation;
+        VendorBuySellList = parent.VendorBuySellList;
+        MerchantContainer = parent.MerchantContainer;
+        Conditions = parent.Conditions;
     }
 
     public void SetFaction(Faction faction) {
@@ -108,5 +156,37 @@ public sealed class EditableFaction : Faction {
         
         for (var i = 0; i < Ranks.Count; i++) Ranks[i].Number = (uint) i;
         faction.Ranks.SetTo(Ranks);
+        
+        faction.CrimeValues = CrimeGoldUseDefaults ? GetDefaultCrimeValues() : CrimeValues;
+        faction.JailOutfit = JailOutfit;
+        faction.SharedCrimeFactionList = SharedCrimeFactionList;
+        faction.ExteriorJailMarker = ExteriorJailMarker;
+        faction.PlayerInventoryContainer = PlayerInventoryContainer;
+        faction.StolenGoodsContainer = StolenGoodsContainer;
+        
+        faction.VendorValues = VendorValues;
+        faction.VendorLocation = VendorLocation;
+        faction.VendorBuySellList = VendorBuySellList;
+        faction.MerchantContainer = MerchantContainer;
+        faction.Conditions = Conditions;
+    }
+    
+    private static CrimeValues GetDefaultCrimeValues() {
+        return new CrimeValues {
+            Arrest = true,
+            AttackOnSight = true,
+            Murder = 1000,
+            Assault = 40,
+            Pickpocket = 25,
+            Trespass = 5,
+            StealMult = 0.5f,
+            Escape = 100,
+            Werewolf = 1000
+        };
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
