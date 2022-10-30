3f990c40128166cda931ae8197cc4717251d1e00
nickp <nickposer2102@gmail.com>
2022-10-30 22:00:00 +0100

    Changed project names from GUI to WPF

---
+++ CreationEditor.WPF.Skyrim/Models/Records/Subrecords/EditableCondition.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+namespace CreationEditor.GUI.Skyrim.Models.Records; 
+
+public sealed class EditableCondition : Condition, INotifyPropertyChanged {
+    public override ConditionData Data { get; set; }
+    
+    public bool Or {
+        get => (Flags & Flag.OR) != 0;
+        set {
+            Flags = Flags.SetFlag(Flag.OR, value);
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool UseGlobal {
+        get => (Flags & Flag.UseGlobal) != 0;
+        set {
+            Flags = Flags.SetFlag(Flag.UseGlobal, value);
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool ParametersUseAliases {
+        get => (Flags & Flag.ParametersUseAliases) != 0;
+        set {
+            Flags = Flags.SetFlag(Flag.ParametersUseAliases, value);
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool UsePackData {
+        get => (Flags & Flag.UsePackData) != 0;
+        set {
+            Flags = Flags.SetFlag(Flag.UsePackData, value);
+            OnPropertyChanged();
+        }
+    }
+    
+    public bool SwapSubjectAndTarget {
+        get => (Flags & Flag.SwapSubjectAndTarget) != 0;
+        set {
+            Flags = Flags.SetFlag(Flag.SwapSubjectAndTarget, value);
+            OnPropertyChanged();
+        }
+    }
+    
+    public EditableCondition(ICondition parent) {
+        
+    }
+
+    public void SetCondition(Condition condition) {
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ CreationEditor.WPF/Views/Test.cs
@@ -0,0 +1,51 @@
+﻿namespace CreationEditor.GUI.Views;
+
+public class Test {
+    public struct vec3 {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    public struct ReferenceTransform {
+        public vec3 translation;
+        public vec3 scale;
+        public vec3 rotations;
+    }
+
+    public struct ReferenceLoad {
+        public string formKey;
+        public string path;
+        public ReferenceTransform transform;
+    }
+
+    public enum UpdateType {
+        TRANSFORM,
+        PATH
+    }
+
+    public struct ReferenceUpdate {
+        public UIntPtr formKey;
+        public UpdateType update;
+        public string path;
+    }
+
+    [System.Runtime.InteropServices.DllImport("TGInterOp.dll")]
+    public static extern UIntPtr loadReferences(uint count, ReferenceLoad load);
+
+    [System.Runtime.InteropServices.DllImport("TGInterOp.dll")]
+    public static extern bool updateReferences(uint count, ReferenceUpdate keys);
+
+    [System.Runtime.InteropServices.DllImport("TGInterOp.dll")]
+    public static extern bool hideReferences(uint count, UIntPtr keys);
+
+    [System.Runtime.InteropServices.DllImport("TGInterOp.dll")]
+    public static extern bool deleteReferences(uint count, UIntPtr keys);
+
+    public Test() {
+        Console.WriteLine("Test123: " + loadReferences(123, new ReferenceLoad { formKey = "test", path = "test.nif", transform = new ReferenceTransform { translation = new vec3(), scale = new vec3(), rotations = new vec3() } }));
+        Console.WriteLine("Test123: " + updateReferences(123, new ReferenceUpdate {formKey = new UIntPtr(), path = "test.nif", update = UpdateType.PATH}));
+        Console.WriteLine("Test123: " + hideReferences(123, UIntPtr.MinValue));
+        Console.WriteLine("Test123: " + deleteReferences(123, UIntPtr.MinValue));
+    }
+}
