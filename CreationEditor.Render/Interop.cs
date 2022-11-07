f57011a99768868493663fb6810831b686bc2f7f
nickp <nickposer2102@gmail.com>
2022-11-07 22:27:55 +0100

    Rebased onto master

---
+++ CreationEditor.Render/Interop.cs
@@ -0,0 +1,96 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+namespace CreationEditor.Render;
+
+public static class Interop {
+    private const string DllName = "TGInterOp.dll";
+
+    public struct ReferenceTransform {
+        public Vector3 Translation;
+        public Vector3 Scale;
+        public Vector3 Rotations;
+    }
+
+    public struct ReferenceLoad {
+        public string FormKey;
+        public string Path;
+        public ReferenceTransform Transform;
+    }
+
+    public enum UpdateType {
+        Transform,
+        Path,
+    }
+
+    public struct ReferenceUpdate {
+        public string FormKey;
+        public UpdateType Update;
+        public string Path;
+    }
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void LoadCallback(uint count, ReferenceLoad[] load);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void UpdateCallback (uint count, ReferenceUpdate[] load);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void HideCallback (uint count, string[] keys, bool hide);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void DeleteCallback (uint count, string[] keys, bool hide);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool addLoadCallback(LoadCallback callback);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool addUpdateCallback(UpdateCallback callback);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool addHideCallback(HideCallback callback);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool addDeleteCallback(DeleteCallback  callback);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern void loadReferences(uint count, ReferenceLoad[] load);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool updateReferences(uint count, ReferenceUpdate[] keys);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool hideReferences(uint count, string[] formKeys, bool hide);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool deleteReferences(uint count, string[] formKeys);
+
+    [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    public static extern bool initTGEditor(int count, string[] strings);
+
+    public static void Start() {
+        Task.Run(() => {
+            initTGEditor(0, Array.Empty<string>());
+        });
+        
+        addLoadCallback((callbackCount, callbackLoads) => {
+            Console.WriteLine($"CALLBACK: {callbackCount}");
+            if (callbackCount == 0) return;
+            
+            foreach (var load in callbackLoads) {
+                Console.WriteLine($"CALLBACK: {load}");
+            }
+        });
+
+        loadReferences(1, new ReferenceLoad[] {
+            new() {
+                FormKey = "123456:Skyrim.esm",
+                Path = "test.nif",
+                Transform = new ReferenceTransform {
+                    Translation = new Vector3(1, 2, 3),
+                    Scale = new Vector3(2, 3, 3),
+                    Rotations = new Vector3()
+                }
+            }
+        });
+    }
+}
---
+++ CreationEditor.Render/InteropTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+using Xunit;
+namespace CreationEditor.Render; 
+
+//Must be run in debug mode to include dll
+public class InteropTest {
+    private readonly Interop.ReferenceLoad _testReference = new() {
+        FormKey = "123456:Skyrim.esm",
+        Path = "test.nif",
+        Transform = new Interop.ReferenceTransform {
+            Translation = new Vector3(1, 2, 3),
+            Scale = new Vector3(2, 3, 3),
+            Rotations = new Vector3()
+        }
+    };
+
+    [Fact]
+    public async void TestInit() {
+        var task = Record.ExceptionAsync(() =>  Task.Run(() => {
+            Interop.initTGEditor(0, Array.Empty<string>());
+        }));
+
+        await Task.Delay(100);
+        
+        Assert.Null(task.Exception);
+    }
+
+    [Fact]
+    public void TestLoadRefs() {
+        var referenceLoads = new[] { _testReference };
+        var count = Convert.ToUInt32(referenceLoads.Length);
+
+        Assert.True(Interop.addLoadCallback((callbackCount, callbackLoads) => {
+            Assert.Equal(count, callbackCount);
+            for (var i = 0; i < callbackLoads.Length; i++) {
+                Assert.Equal(callbackLoads[i].FormKey, referenceLoads[i].FormKey);
+            }
+        }));
+        
+        Interop.loadReferences(count, referenceLoads);
+    }
+}
