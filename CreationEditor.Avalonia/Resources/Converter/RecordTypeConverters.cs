ee85dff37cf5bf8c15c75f33757b7610228c43de
nickp <nickposer2102@gmail.com>
2023-01-06 01:32:05 +0100

    Added record type converters

---
+++ CreationEditor.Avalonia/Resources/Converter/RecordTypeConverters.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using Loqui;
+namespace CreationEditor.Avalonia.Converter; 
+
+public class RecordTypeConverters {
+    public static readonly FuncValueConverter<Type, string?> ToName
+        = new(type => {
+            if (type == null) return null;
+            
+            return LoquiRegistration.TryGetRegister(type, out var register)
+                ? register.ClassType.Name
+                : type.Name;
+        });
+    
+    public static readonly FuncValueConverter<Type, string?> ToGame
+        = new(type => {
+            if (type == null) return null;
+
+            return LoquiRegistration.TryGetRegister(type, out var registration)
+                ? registration.ProtocolKey.Namespace
+                : string.Empty;
+        });
+}
