ca286f7378f2b5143ac31ea967a6bf69bb38fa79
nickp <nickposer2102@gmail.com>
2023-01-19 01:50:09 +0100

    Removed template disposable clearing on unload because it caused subscriptions for form key picker to vanish indefinitely

--- CreationEditor.Avalonia/FormKeyPicker/DisposableTemplatedControl.cs
+++ CreationEditor.Avalonia/FormKeyPicker/DisposableTemplatedControl.cs
@@ -7,22 +7,11 @@ public class DisposableTemplatedControl : TemplatedControl {
     protected readonly IDisposableBucket TemplateDisposable = new DisposableBucket();
 
     public DisposableTemplatedControl() {
-        Loaded += (_, _) => OnLoaded();
-        Loaded += (_, _) => {
-            if (Template == null) return;
-
-            TemplateDisposable.Clear();
-        };
-        Unloaded += (_, _) => {
-            TemplateDisposable.Clear();
-            UnloadDisposable.Clear();
-        };
+        Unloaded += (_, _) => UnloadDisposable.Clear();
     }
-
+    
     protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
         base.OnApplyTemplate(e);
         TemplateDisposable.Clear();
     }
-
-    protected override void OnLoaded() {}
 }
