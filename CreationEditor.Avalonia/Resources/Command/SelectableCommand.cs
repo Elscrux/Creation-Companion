6ab130ee3d76af568c37d3b97d725c80f711bf19
nickp <nickposer2102@gmail.com>
2023-01-19 01:48:59 +0100

    Added generic selectable commands

---
+++ CreationEditor.Avalonia/Resources/Command/SelectableCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive;
+using CreationEditor.Avalonia.Models;
+using ReactiveUI;
+namespace CreationEditor.Avalonia.Command; 
+
+public static class SelectableCommand {
+    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>, Unit> SelectAll =
+        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>>(selectables => {
+            foreach (var selectable in selectables) {
+                selectable.IsSelected = true;
+            }
+        });
+    
+    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>, Unit> UnselectAll =
+        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>>(selectables => {
+            foreach (var selectable in selectables) {
+                selectable.IsSelected = false;
+            }
+        });
+    
+    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>, Unit> ToggleAll =
+        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>>(selectables => {
+            foreach (var selectable in selectables) {
+                selectable.IsSelected = !selectable.IsSelected;
+            }
+        });
+}
