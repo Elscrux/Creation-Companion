5fa2e49c347963818b539bd12d39d815f67196f5
nickp <nickposer2102@gmail.com>
2023-01-19 01:28:50 +0100

    Moved default columns to use the extra columns system

---
+++ CreationEditor.Avalonia/Models/Record/List/ExtraColumns/MajorRecordExtraColumns.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data;
+using CreationEditor.Avalonia.Comparer;
+using Mutagen.Bethesda.Plugins.Records;
+namespace CreationEditor.Avalonia.Models.Record.List.ExtraColumns; 
+
+public class MajorRecordExtraColumns : ExtraColumns<IMajorRecordGetter> {
+    public override IEnumerable<ExtraColumn> Columns {
+        get {
+            yield return new ExtraColumn(
+                new DataGridTextColumn {
+                    Header = "EditorID",
+                    Binding = new Binding("Record.EditorID", BindingMode.OneWay),
+                    CanUserSort = true,
+                    CustomSortComparer = RecordComparers.EditorIDComparer,
+                    Width = new DataGridLength(200),
+                },
+                220);
+            
+            yield return new ExtraColumn(
+                new DataGridTextColumn {
+                    Header = "FormKey",
+                    Binding = new Binding("Record.FormKey", BindingMode.OneWay),
+                    CanUserSort = true,
+                    CustomSortComparer = RecordComparers.FormKeyComparer,
+                    Width = new DataGridLength(100),
+                },
+                210);
+
+            yield return new ExtraColumn(
+                new DataGridTextColumn {
+                    Header = "Use Info",
+                    Binding = new Binding("References.Count", BindingMode.OneWay),
+                    CanUserSort = true,
+                    Width = new DataGridLength(90),
+                },
+                200);
+        }
+    }
+}
