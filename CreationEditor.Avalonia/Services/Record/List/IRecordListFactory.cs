f9eecf1cecda95aedd75cfdfddb0987430b5adcc
nickp <nickposer2102@gmail.com>
2023-01-19 01:44:05 +0100

    record browser renaming leftover

--- CreationEditor.Avalonia/Services/Record/List/IRecordListFactory.cs
+++ CreationEditor.Avalonia/Services/Record/List/IRecordListFactory.cs
@@ -1,7 +1,7 @@
-﻿using CreationEditor.Avalonia.Models.Record.Browser;
+﻿using CreationEditor.Avalonia.ViewModels.Record.Browser;
 using CreationEditor.Avalonia.Views.Record;
 namespace CreationEditor.Avalonia.Services.Record.List;
 
 public interface IRecordListFactory {
-    public RecordList FromType(Type type, IRecordBrowserSettings? browserSettings = null);
+    public RecordList FromType(Type type, IRecordBrowserSettingsVM? browserSettings = null);
 }
