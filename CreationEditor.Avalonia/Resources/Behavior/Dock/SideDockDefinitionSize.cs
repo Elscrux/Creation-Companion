44148266138f9c12bad77a47d7c47fa3c029bb5d
nickp <nickposer2102@gmail.com>
2023-01-21 20:03:44 +0100

    Fixed side dock resizing

--- CreationEditor.Avalonia/Resources/Behavior/Dock/SideDockDefinitionSize.cs
+++ CreationEditor.Avalonia/Resources/Behavior/Dock/SideDockDefinitionSize.cs
@@ -33,8 +33,8 @@ public sealed class SideDockDefinitionSize : Behavior<DefinitionBase> {
 
         AssociatedObject.GetObservable(GetSizeProperty())
             .Subscribe(size => {
-                if (UpdateSize && PreviousActiveTab != null && SideDock.ActiveTab != null) {
-                    PreviousActiveTab.Size = size.Value;
+                if (UpdateSize && SideDock.ActiveTab != null) {
+                    SideDock.ActiveTab.Size = size.Value;
                 }
             });
         
