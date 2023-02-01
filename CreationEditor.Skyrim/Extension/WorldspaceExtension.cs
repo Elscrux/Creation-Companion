7730aa3bfb433de07e40ea836bd08eed7a556ec5
nickp <nickposer2102@gmail.com>
2023-02-02 00:00:33 +0100

    Added utility function to enumerate all cells in all versions of a worldspace in all mods

--- CreationEditor.Skyrim/Extension/WorldspaceExtension.cs
+++ CreationEditor.Skyrim/Extension/WorldspaceExtension.cs
@@ -1,4 +1,6 @@
-﻿using Mutagen.Bethesda.Skyrim;
+﻿using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Skyrim;
 namespace CreationEditor.Skyrim.Extension; 
 
 public static class WorldspaceExtension {
@@ -8,6 +10,20 @@ public static class WorldspaceExtension {
                 foreach (var cell in subBlock.Items) {
                     yield return cell;
                 }
+            }
+        }
+    }
+    
+    public static IEnumerable<ICellGetter> EnumerateAllCells(this ILinkCache linkCache, FormKey worldspaceFormKey) {
+        var visitedCells = new HashSet<FormKey>();
+        
+        foreach (var worldspace in linkCache.ResolveAll<IWorldspaceGetter>(worldspaceFormKey)) {
+            foreach (var cell in worldspace.EnumerateCells()) {
+                var cellFormKey = cell.FormKey;
+                if (visitedCells.Contains(cellFormKey)) continue;
+
+                visitedCells.Add(cellFormKey);
+                yield return cell;
             }
         }
     }
