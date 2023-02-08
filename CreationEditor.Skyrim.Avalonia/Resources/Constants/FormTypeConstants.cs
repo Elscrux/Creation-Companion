8f04ce85a6c70d269c670d0ccfe40a51077294cf
nickp <nickposer2102@gmail.com>
2023-02-08 15:56:11 +0100

    Use direct relatable interface type

--- CreationEditor.Skyrim.Avalonia/Resources/Constants/FormTypeConstants.cs
+++ CreationEditor.Skyrim.Avalonia/Resources/Constants/FormTypeConstants.cs
@@ -5,7 +5,7 @@ using Noggog;
 namespace CreationEditor.Skyrim.Avalonia.Resources.Constants; 
 
 public static class FormTypeConstants {
-    public static readonly IEnumerable<Type> RelationTypes = new[] { typeof(IFactionGetter), typeof(IRaceGetter) };
+    public static readonly IEnumerable<Type> RelatableTypes = typeof(IRelatableGetter).AsEnumerable();
     public static readonly IEnumerable<Type> OutfitTypes = typeof(IOutfitGetter).AsEnumerable();
     public static readonly IEnumerable<Type> FormListTypes = typeof(IFormListGetter).AsEnumerable();
     public static readonly IEnumerable<Type> WorldspaceTypes = typeof(IWorldspaceGetter).AsEnumerable();
