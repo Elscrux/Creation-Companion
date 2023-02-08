9c3b4d560fbc79057ee7a9b06cd7dc8b0e1c516e
nickp <nickposer2102@gmail.com>
2023-02-08 16:04:10 +0100

    Add alternate way to detect popup clicks as the command inside the item template cannot bind correctly

--- CreationEditor.Avalonia/FormKeyPicker/FormKeyPicker.axaml.cs
+++ CreationEditor.Avalonia/FormKeyPicker/FormKeyPicker.axaml.cs
@@ -1,32 +1,50 @@
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.VisualTree;
 using Mutagen.Bethesda.Plugins.Records;
-using ReactiveUI;
 namespace CreationEditor.Avalonia.FormKeyPicker;
 
 // Ported from Mutagen.Bethesda.WPF by Noggog
+[TemplatePart(Name = "PART_Popup", Type = typeof(Popup))]
 public class FormKeyPicker : AFormKeyPicker {
     public double MaxSearchBoxHeight {
         get => GetValue(MaxSearchBoxHeightProperty);
         set => SetValue(MaxSearchBoxHeightProperty, value);
     }
-    public static readonly StyledProperty<double> MaxSearchBoxHeightProperty = AvaloniaProperty.Register<FormKeyPicker, double>(nameof(MaxSearchBoxHeight), 1000d);
+    public static readonly StyledProperty<double> MaxSearchBoxHeightProperty = AvaloniaProperty.Register<FormKeyPicker, double>(nameof(MaxSearchBoxHeight), 500d);
 
     public double SearchBoxHeight {
         get => GetValue(SearchBoxHeightProperty);
         set => SetValue(SearchBoxHeightProperty, value);
     }
     public static readonly StyledProperty<double> SearchBoxHeightProperty = AvaloniaProperty.Register<FormKeyPicker, double>(nameof(SearchBoxHeight), double.NaN);
+    
+    private Popup? _popup;
 
-    static FormKeyPicker() {
-        // DefaultStyleKeyProperty.OverrideMetadata(typeof(FormKeyPicker), new FrameworkPropertyMetadata(typeof(FormKeyPicker))); todo
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+        base.OnApplyTemplate(e);
+        
+        _popup = e.NameScope.Find<Popup>("PART_Popup");
     }
 
-    public FormKeyPicker() {
-        PickerClickCommand = ReactiveCommand.Create((object o) => {
-            if (o is not IMajorRecordIdentifier identifier) return;
+    protected override void OnPointerReleased(PointerReleasedEventArgs e) {
+        base.OnPointerReleased(e);
 
-            FormKey = identifier.FormKey;
-            InSearchMode = false;
-        });
+        if (e is not { Handled: false, Source: IVisual source }) return;
+        if (_popup?.IsInsidePopup(source) is not true) return;
+
+        var border = source.FindAncestorOfType<Border>(true);
+        if (border?.DataContext is not IMajorRecordIdentifier identifier) return;
+
+        FormKey = identifier.FormKey;
+        
+        // Unfocus twice to ensure the text box doesn't get focus again
+        FocusManager.Instance?.Focus(null);
+        FocusManager.Instance?.Focus(null);
+        
+        InSearchMode = false;
     }
 }
