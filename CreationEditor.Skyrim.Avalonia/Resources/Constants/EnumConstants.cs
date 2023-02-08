e24b27c8113d48920113e36efe92bb774168ced4
nickp <nickposer2102@gmail.com>
2023-02-08 15:55:53 +0100

    Add explicit combat reaction ordering

--- CreationEditor.Skyrim.Avalonia/Resources/Constants/EnumConstants.cs
+++ CreationEditor.Skyrim.Avalonia/Resources/Constants/EnumConstants.cs
@@ -4,5 +4,10 @@ using Mutagen.Bethesda.Skyrim;
 namespace CreationEditor.Skyrim.Avalonia.Resources.Constants; 
 
 public static class EnumConstants {
-    public static readonly IEnumerable<CombatReaction> CombatReactions = Enum.GetValues<CombatReaction>();
+    public static readonly IEnumerable<CombatReaction> CombatReactions = new[] {
+        CombatReaction.Ally,
+        CombatReaction.Friend,
+        CombatReaction.Neutral,
+        CombatReaction.Enemy,
+    };
 }
