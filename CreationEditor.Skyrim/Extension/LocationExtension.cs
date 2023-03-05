b9167d9bff444f84e934c9833e1d33c8abc5d0c7
nickp <nickposer2102@gmail.com>
2023-03-06 00:18:10 +0100

    More record extensions

---
+++ CreationEditor.Skyrim/Extension/LocationExtension.cs
@@ -0,0 +1,18 @@
+﻿using Mutagen.Bethesda;
+using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Skyrim;
+namespace CreationEditor.Skyrim.Extension; 
+
+public static class LocationExtension {
+    public static IEnumerable<ILocationGetter> GetAllParentLocations(this ILocationGetter location, ILinkCache linkCache) {
+        var currentLocation = location;
+        while (!currentLocation.ParentLocation.IsNull) {
+            if (currentLocation.ParentLocation.TryResolve(linkCache, out var parentLocation)) {
+                yield return parentLocation;
+                currentLocation = parentLocation;
+            } else {
+                yield break;
+            }
+        }
+    }
+}
---
+++ CreationEditor.Skyrim/Extension/NpcExtension.cs
@@ -0,0 +1,9 @@
+﻿using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Skyrim;
+namespace CreationEditor.Skyrim.Extension; 
+
+public static class NpcExtension {
+    public static MaleFemaleGender GetGender(this INpcGetter npc) {
+        return (npc.Configuration.Flags & NpcConfiguration.Flag.Female) == 0 ? MaleFemaleGender.Male : MaleFemaleGender.Female;
+    }
+}
