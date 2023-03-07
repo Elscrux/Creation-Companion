95304cd2f5168307d524112b5dde687947f07525
nickp <nickposer2102@gmail.com>
2023-03-07 21:26:43 +0100

    Add null handling

--- CreationEditor.Avalonia/Resources/Command/SelectableCommand.cs
+++ CreationEditor.Avalonia/Resources/Command/SelectableCommand.cs
@@ -1,25 +1,31 @@
 ﻿using System.Reactive;
 using CreationEditor.Avalonia.Models;
 using ReactiveUI;
-namespace CreationEditor.Avalonia.Command; 
+namespace CreationEditor.Avalonia.Command;
 
 public static class SelectableCommand {
-    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>, Unit> SelectAll =
-        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>>(selectables => {
+    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>?, Unit> SelectAll =
+        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>?>(selectables => {
+            if (selectables == null) return;
+
             foreach (var selectable in selectables) {
                 selectable.IsSelected = true;
             }
         });
-    
-    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>, Unit> UnselectAll =
-        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>>(selectables => {
+
+    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>?, Unit> UnselectAll =
+        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>?>(selectables => {
+            if (selectables == null) return;
+
             foreach (var selectable in selectables) {
                 selectable.IsSelected = false;
             }
         });
-    
-    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>, Unit> ToggleAll =
-        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>>(selectables => {
+
+    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>?, Unit> ToggleAll =
+        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>?>(selectables => {
+            if (selectables == null) return;
+
             foreach (var selectable in selectables) {
                 selectable.IsSelected = !selectable.IsSelected;
             }
