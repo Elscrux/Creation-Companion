27889e0383134139d6f2ac5cadd2f1e183412e74
nickp <nickposer2102@gmail.com>
2023-03-09 00:22:55 +0100

    Fix spell filter and remove addiction type

--- CreationEditor.Skyrim.Avalonia/Resources/Constants/EnumConstants.cs
+++ CreationEditor.Skyrim.Avalonia/Resources/Constants/EnumConstants.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
 namespace CreationEditor.Skyrim.Avalonia.Resources.Constants;
 
 public static class EnumConstants {
+    public static readonly IEnumerable<SpellType> SpellTypes = Enum.GetValues<SpellType>().Except(SpellType.Addiction.AsEnumerable());
+
     public static readonly IEnumerable<CombatReaction> CombatReactions = new[] {
         CombatReaction.Ally,
         CombatReaction.Friend,
--- CreationEditor.Skyrim.Avalonia/Services/Record/Browser/Filter/SpellFilter.cs
+++ CreationEditor.Skyrim.Avalonia/Services/Record/Browser/Filter/SpellFilter.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using CreationEditor.Avalonia.Models.Record.Browser;
 using CreationEditor.Avalonia.Services.Record.Browser.Filter;
+using CreationEditor.Skyrim.Avalonia.Resources.Constants;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
 namespace CreationEditor.Skyrim.Avalonia.Services.Record.Browser.Filter;
 
 public class SpellFilter : RecordFilter<ISpellGetter> {
     public override IEnumerable<RecordFilterListing> GetListings(Type type) {
-        return Enum.GetNames<SpellType>()
-            .Select(spellType => new RecordFilterListing(
-                spellType,
-                record => record.Type.ToString() == spellType));
+        return EnumConstants.SpellTypes
+            .Select(spellType => new RecordFilterListing(spellType.ToString(),
+                record => record is ISpellGetter spell && spell.Type == spellType));
     }
 }
