79169b1baad9d81d9af5304bfb6de0c5362d2d6f
nickp <nickposer2102@gmail.com>
2023-03-09 16:32:49 +0100

    Add window close command

---
+++ CreationEditor.Avalonia/Resources/Command/WindowCommands.cs
@@ -0,0 +1,9 @@
+﻿using System.Reactive;
+using Avalonia.Controls;
+using ReactiveUI;
+namespace CreationEditor.Avalonia.Command; 
+
+public class WindowCommands {
+    public static readonly ReactiveCommand<Window?, Unit> Close =
+        ReactiveCommand.Create<Window?>(window => window?.Close());
+}
