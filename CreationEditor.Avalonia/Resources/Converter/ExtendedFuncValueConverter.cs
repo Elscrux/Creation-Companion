1ce95254cccca638e0f14b0513fd3fc3a34b68d0
nickp <nickposer2102@gmail.com>
2023-03-11 13:16:08 +0100

    Add extended FuncValueConverter that has a convert back function

---
+++ CreationEditor.Avalonia/Resources/Converter/ExtendedFuncValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Utilities;
+namespace CreationEditor.Avalonia.Converter;
+
+public class ExtendedFuncValueConverter<TIn, TOut> : IValueConverter {
+    private readonly Func<TIn?, TOut> _convert;
+    private readonly Func<TOut?, TIn> _convertBack;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ExtendedFuncValueConverter{TIn, TOut}"/> class.
+    /// </summary>
+    /// <param name="convert">The convert function.</param>
+    /// <param name="convertBack">The convert back function</param>
+    public ExtendedFuncValueConverter(Func<TIn?, TOut> convert, Func<TOut?, TIn> convertBack) {
+        _convert = convert;
+        _convertBack = convertBack;
+    }
+
+    /// <inheritdoc/>
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return TypeUtilities.CanCast<TIn>(value)
+            ? _convert((TIn?) value)
+            : AvaloniaProperty.UnsetValue;
+    }
+
+    /// <inheritdoc/>
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        return TypeUtilities.CanCast<TOut>(value)
+            ? _convertBack((TOut?) value)
+            : AvaloniaProperty.UnsetValue;
+    }
+}
