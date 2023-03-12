ec726795909f0a20f8a14783195e03b5155c9153
nickp <nickposer2102@gmail.com>
2023-03-12 13:29:41 +0100

    Add setting documentation

--- CreationEditor/Services/Settings/ISetting.cs
+++ CreationEditor/Services/Settings/ISetting.cs
@@ -1,9 +1,18 @@
 ﻿namespace CreationEditor.Services.Settings;
 
 public interface ISetting {
+    /// <summary>
+    /// Name of the setting
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// Parent setting class type
+    /// </summary>
     public Type? Parent { get; }
 
+    /// <summary>
+    /// Is automatically filled by settings provider
+    /// </summary>
     public List<ISetting> Children { get; }
 }
