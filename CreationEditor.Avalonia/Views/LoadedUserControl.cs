6e5470bcf4fbd6e64e8d0a897ac34155981cb526
nickp <nickposer2102@gmail.com>
2023-03-18 17:59:15 +0100

    UserControl parent class for loading

---
+++ CreationEditor.Avalonia/Views/LoadedUserControl.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using Noggog;
+namespace CreationEditor.Avalonia.Views; 
+
+public abstract class LoadedUserControl : UserControl {
+    protected readonly IDisposableBucket UnloadDisposable = new DisposableBucket();
+
+    protected override void OnUnloaded() {
+        base.OnUnloaded();
+
+        UnloadDisposable.Clear();
+    }
+}
