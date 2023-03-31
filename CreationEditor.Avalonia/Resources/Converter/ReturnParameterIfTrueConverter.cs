556a9cec99f0bafffc45a2a06bb33be8996d94ff
nickp <nickposer2102@gmail.com>
2023-03-31 23:21:14 +0200

    Add converted default value

--- CreationEditor.Avalonia/Resources/Converter/ReturnParameterIfTrueConverter.cs
+++ CreationEditor.Avalonia/Resources/Converter/ReturnParameterIfTrueConverter.cs
@@ -5,6 +5,7 @@ namespace CreationEditor.Avalonia.Converter;
 public sealed class ReturnParameterIfTrueConverter<TIn> : IValueConverter
     where TIn : IParsable<TIn> {
     public TIn? DefaultValue { get; set; }
+    public object? DefaultValueConverted { get; set; }
     public IFormatProvider? ParseFormatProvider { get; set; } = null;
     public IValueConverter? Converter { get; set; }
 
@@ -12,7 +13,9 @@ public sealed class ReturnParameterIfTrueConverter<TIn> : IValueConverter
         var parsedValue = value is true ? TIn.Parse(parameter as string ?? string.Empty, ParseFormatProvider) : DefaultValue;
         if (Converter == null) return parsedValue;
 
-        return Converter?.Convert(parsedValue, targetType, null, CultureInfo.CurrentCulture);
+        if (value is not true && DefaultValueConverted != null) return DefaultValueConverted;
+
+        return Converter?.Convert(parsedValue, targetType, null, CultureInfo.CurrentCulture) ?? DefaultValueConverted;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
@@ -22,12 +25,16 @@ public sealed class ReturnParameterIfTrueConverter<TIn> : IValueConverter
 public sealed class ReturnParameterIfTrueMultiConverter<TIn> : IMultiValueConverter
     where TIn : IParsable<TIn> {
     public TIn? DefaultValue { get; set; }
+    public object? DefaultValueConverted { get; set; }
     public IFormatProvider? ParseFormatProvider { get; set; } = null;
     public IValueConverter? Converter { get; set; }
 
     public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture) {
-        var parsedValue = values.All(x => x is true) ? TIn.Parse(parameter as string ?? string.Empty, ParseFormatProvider) : DefaultValue;
+        var pass = values.All(x => x is true);
+        var parsedValue = pass ? TIn.Parse(parameter as string ?? string.Empty, ParseFormatProvider) : DefaultValue;
         if (Converter == null) return parsedValue;
+
+        if (pass is not true && DefaultValueConverted != null) return DefaultValueConverted;
 
         return Converter?.Convert(parsedValue, targetType, null, CultureInfo.CurrentCulture);
     }
