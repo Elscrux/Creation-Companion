bc0e9404790a9e17398261a1edc78e085aa879cd
nickp <nickposer2102@gmail.com>
2023-03-31 23:22:15 +0200

    Add params version of AnyInheritsFarm

--- CreationEditor/Extension/TypeExtension.cs
+++ CreationEditor/Extension/TypeExtension.cs
@@ -2,7 +2,11 @@
 namespace CreationEditor;
 
 public static class TypeExtension {
-    public static bool AnyInheritsFrom(this IEnumerable<Type> types, Type type) {
-        return types.Any(t => t.InheritsFrom(type));
+    public static bool AnyInheritsFrom(this IEnumerable<Type> types, Type inheritedType) {
+        return types.Any(type => type.InheritsFrom(inheritedType));
+    }
+
+    public static bool AnyInheritsFrom(this IEnumerable<Type> types, params Type[] inheritedTypes) {
+        return types.Any(type => inheritedTypes.Any(inheritedType => type.InheritsFrom(inheritedType)));
     }
 }
