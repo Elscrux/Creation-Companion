a24417ccd7cac3458b55a00493ce618d1fde9369
nickp <nickposer2102@gmail.com>
2023-04-01 16:46:44 +0200

    Add toggleable radio button

---
+++ CreationEditor.Avalonia/Views/Basic/ToggleRadioButton.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+namespace CreationEditor.Avalonia.Views.Basic; 
+
+public class ToggleRadioButton : RadioButton {
+    protected override void OnClick() {
+        if (IsChecked == true) {
+            IsChecked = false;
+        } else {
+            base.OnClick();
+        }
+    }
+}
