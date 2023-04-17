697d7287e9d8941dfc9ee67c95c2cd121a82bdcc
nickp <nickposer2102@gmail.com>
2023-04-17 19:46:51 +0200

    Add mutable datagrid

--- CreationEditor.Avalonia/Resources/Attached/ListShortcuts.cs
+++ CreationEditor.Avalonia/Resources/Attached/ListShortcuts.cs
@@ -4,10 +4,15 @@ using Avalonia;
 using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
+using Avalonia.VisualTree;
+using FluentAvalonia.UI.Controls;
 using Noggog;
-using ReactiveUI;
 namespace CreationEditor.Avalonia.Attached;
 
 public sealed class ListShortcuts : AvaloniaObject {
@@ -20,14 +25,23 @@ public sealed class ListShortcuts : AvaloniaObject {
     public static ICommand GetRemove(AvaloniaObject element) => element.GetValue(RemoveProperty);
     public static void SetRemove(AvaloniaObject element, ICommand parameter) => element.SetValue(RemoveProperty, parameter);
 
-    private static readonly KeyGesture AddGesture = KeyGesture.Parse("Add");
-    private static readonly KeyGesture RemoveGesture = KeyGesture.Parse("Delete");
+    private static readonly KeyGesture AddGesture = new(Key.OemPlus);
+    private static readonly KeyGesture RemoveGesture = new(Key.OemMinus);
 
     private const string AddHeader = "Add";
     private const string RemoveHeader = "Remove";
 
+    private const Symbol AddSymbol = Symbol.Add;
+    private const Symbol RemoveSymbol = Symbol.Remove;
+
     static ListShortcuts() {
-        AddProperty.Changed.Subscribe(args => HandleGesture(args, null, AddGesture, AddHeader));
+        AddProperty.Changed.Subscribe(args => {
+            HandleGesture(args, null, AddGesture, AddHeader, AddSymbol);
+
+            if (args.Sender is DataGrid dg) {
+                ToggleAddButton(dg, args);
+            }
+        });
 
         RemoveProperty.Changed.Subscribe(args => {
             var selectedItems = args.Sender switch {
@@ -36,11 +50,64 @@ public sealed class ListShortcuts : AvaloniaObject {
                 _ => null
             };
 
-            HandleGesture(args, selectedItems, RemoveGesture, RemoveHeader);
+            HandleGesture(args, selectedItems, RemoveGesture, RemoveHeader, RemoveSymbol);
+
+            if (args.Sender is DataGrid dg) {
+                AddRemoveButton(dg, args.NewValue.GetValueOrDefault());
+            }
         });
     }
 
-    private static void HandleGesture(AvaloniaPropertyChangedEventArgs<ICommand> args, IList? selectedItems, KeyGesture gesture, string header) {
+    private static void ToggleAddButton(DataGrid dg, AvaloniaPropertyChangedEventArgs<ICommand> args) {
+        dg.Loaded -= Function;
+        dg.Loaded += Function;
+
+        void Function(object? sender, RoutedEventArgs routedEventArgs) {
+            var addButton = dg.FindDescendantOfType<Button>();
+            if (addButton == null) return;
+
+            addButton.IsVisible = true;
+            addButton.Command = args.NewValue.GetValueOrDefault();
+        }
+    }
+
+    private static void AddRemoveButton(DataGrid dg, ICommand? command) {
+        var removeButtonTemplate = new FuncDataTemplate<object>((o, _) => new Button {
+            [!Visual.IsVisibleProperty] = new Binding("IsPointerOver") {
+                RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor) {
+                    AncestorType = typeof(DataGridRow),
+                }
+            },
+            Content = new SymbolIcon { Symbol = Symbol.Delete },
+            Foreground = Brushes.Red,
+            Classes = new Classes("Transparent"),
+            Command = command,
+            HorizontalAlignment = HorizontalAlignment.Left,
+            HorizontalContentAlignment = HorizontalAlignment.Left,
+            CommandParameter = new ArrayList { o },
+        });
+
+        var removeColumn = new DataGridTemplateColumn {
+            CellTemplate = removeButtonTemplate,
+            CanUserResize = false,
+            CanUserSort = false,
+            CanUserReorder = false,
+            IsReadOnly = true,
+        };
+
+        dg.Loaded -= Function;
+        dg.Loaded += Function;
+
+        void Function(object? sender, RoutedEventArgs routedEventArgs) {
+            if (dg.Columns.Any(c =>
+                c is DataGridTemplateColumn templateColumn
+             && templateColumn.CellTemplate == removeButtonTemplate)) return;
+
+            dg.Columns.Add(removeColumn);
+        }
+    }
+
+    private static void HandleGesture(AvaloniaPropertyChangedEventArgs<ICommand> args, IList? selectedItems, KeyGesture gesture, string header, Symbol menuIcon) {
         if (args.Sender is not Control control) return;
 
         var newCommand = args.NewValue.GetValueOrDefault();
@@ -58,21 +125,22 @@ public sealed class ListShortcuts : AvaloniaObject {
 
         // Add context menu
         control.ContextFlyout ??= new MenuFlyout();
-        AddCommand(selectedItems, header, control.ContextFlyout, newCommand);
+        AddCommand(selectedItems, header, control.ContextFlyout, newCommand, menuIcon);
 
         control.GetPropertyChangedObservable(Control.ContextFlyoutProperty)
             .Subscribe(_ => {
                 control.ContextFlyout ??= new MenuFlyout();
-                AddCommand(selectedItems, header, control.ContextFlyout, newCommand);
+                AddCommand(selectedItems, header, control.ContextFlyout, newCommand, menuIcon);
             });
     }
 
-    private static void AddCommand(IEnumerable? selectedItems, string header, FlyoutBase? flyout, ICommand? newCommand) {
+    private static void AddCommand(IEnumerable? selectedItems, string header, FlyoutBase? flyout, ICommand? newCommand, Symbol menuIcon) {
         if (flyout is not MenuFlyout { Items: IAvaloniaList<object> list }) return;
         if (list.OfType<MenuItem>().Any(x => x.Command == newCommand)) return;
 
         if (newCommand != null) {
             list.Add(new MenuItem {
+                Icon = new SymbolIcon { Symbol = menuIcon },
                 Header = header,
                 Command = newCommand,
                 CommandParameter = selectedItems
