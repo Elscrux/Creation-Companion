a29ec074239268752a05d679fea641a5914b2357
nickp <nickposer2102@gmail.com>
2023-04-17 19:39:33 +0200

    Remove redundant property

--- CreationEditor.Avalonia/Resources/Command/RemoveElement.cs
+++ CreationEditor.Avalonia/Resources/Command/RemoveElement.cs
@@ -1,16 +1,7 @@
 ﻿using System.Collections;
-using Avalonia;
 namespace CreationEditor.Avalonia.Command;
 
 public class RemoveElement<T> : ListCommand<T> where T : new() {
-    public static readonly StyledProperty<IList<T>> RemoveItemsProperty
-        = AvaloniaProperty.Register<RemoveElement<T>, IList<T>>(nameof(RemoveItems));
-
-    public IList<T> RemoveItems {
-        get => GetValue(RemoveItemsProperty);
-        set => SetValue(RemoveItemsProperty, value);
-    }
-
     public override void Execute(object? parameter) {
         if (List == null) return;
         if (parameter is not IList removeList) return;
