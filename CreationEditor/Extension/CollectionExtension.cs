b13d175eea4971c466d691c30d1207913ccd28bd
nickp <nickposer2102@gmail.com>
2023-04-17 19:59:48 +0200

    Add utility collection extension

---
+++ CreationEditor/Extension/CollectionExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace CreationEditor; 
+
+public static class CollectionExtension {
+    public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items) {
+        foreach (var item in items) {
+            collection.Add(item);
+        }
+    }
+}
