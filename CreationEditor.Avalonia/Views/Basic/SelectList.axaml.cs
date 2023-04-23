656b2a67e83c8016b29d4b2bf9c2ae6b80397941
nickp <nickposer2102@gmail.com>
2023-04-23 22:31:10 +0200

    Add SelectList

---
+++ CreationEditor.Avalonia/Views/Basic/SelectList.axaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Metadata;
+namespace CreationEditor.Avalonia.Views.Basic; 
+
+public class SelectList : TemplatedControl {
+    public static readonly StyledProperty<IEnumerable> ItemsProperty
+        = AvaloniaProperty.Register<SelectList, IEnumerable>(nameof(Items));
+
+    public IEnumerable Items {
+        get => GetValue(ItemsProperty);
+        set => SetValue(ItemsProperty, value);
+    }
+
+    public static readonly StyledProperty<IDataTemplate> DataTemplateProperty
+        = AvaloniaProperty.Register<SelectList, IDataTemplate>(nameof(DataTemplate));
+
+    [Content]
+    public IDataTemplate DataTemplate {
+        get => GetValue(DataTemplateProperty);
+        set => SetValue(DataTemplateProperty, value);
+    }
+}
+
