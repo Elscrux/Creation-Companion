9a40a803673164715452365b27da3ad77337f29b
nickp <nickposer2102@gmail.com>
2023-04-25 15:06:28 +0200

    Integrate visual plugins in main vm

--- CreationEditor.Avalonia/ViewModels/MainVM.cs
+++ CreationEditor.Avalonia/ViewModels/MainVM.cs
@@ -2,16 +2,21 @@
 using System.Reactive.Linq;
 using Autofac;
 using Avalonia.Controls;
+using Avalonia.Data.Converters;
 using CreationEditor.Avalonia.Models;
+using CreationEditor.Avalonia.Models.Docking;
 using CreationEditor.Avalonia.Services;
 using CreationEditor.Avalonia.Services.Busy;
 using CreationEditor.Avalonia.Services.Docking;
+using CreationEditor.Avalonia.Services.Plugin;
 using CreationEditor.Avalonia.ViewModels.Mod;
 using CreationEditor.Avalonia.ViewModels.Notification;
 using CreationEditor.Avalonia.ViewModels.Setting;
+using CreationEditor.Avalonia.Views;
 using CreationEditor.Avalonia.Views.Mod;
 using CreationEditor.Avalonia.Views.Setting;
 using CreationEditor.Services.Environment;
+using CreationEditor.Services.Plugin;
 using ReactiveUI;
 namespace CreationEditor.Avalonia.ViewModels;
 
@@ -25,8 +30,11 @@ public sealed class MainVM : ViewModel {
     public IObservable<string> WindowTitleObs { get; }
 
     public IDockingManagerService DockingManagerService { get; }
+    public IPluginService? PluginService { get; }
+    public IList<IVisualPluginDefinition>? VisualPlugins { get; }
 
     public ReactiveCommand<Unit, Unit> OpenSelectMods { get; }
+    public ReactiveCommand<IVisualPluginDefinition, Unit> OpenPlugin { get; }
     public ReactiveCommand<Unit, Unit> OpenSettings { get; }
     public ReactiveCommand<Unit, Unit> Save { get; }
 
@@ -40,13 +48,26 @@ public sealed class MainVM : ViewModel {
         ModSelectionVM modSelectionVM,
         IDockingManagerService dockingManagerService,
         IDockFactory dockFactory,
-        MainWindow mainWindow) {
+        MainWindow mainWindow,
+        IPluginService? pluginService) {
         _modSelectionVM = modSelectionVM;
         NotificationVM = notificationVM;
         BusyService = busyService;
         DockingManagerService = dockingManagerService;
+        PluginService = pluginService;
+        VisualPlugins = PluginService?.Plugins.OfType<IVisualPluginDefinition>().ToList();
 
         OpenSelectMods = ReactiveCommand.Create(ShowModSelection);
+
+        OpenPlugin = ReactiveCommand.Create<IVisualPluginDefinition>(plugin => {
+            DockingManagerService.AddControl(
+                plugin.GetControl(),
+                new DockConfig {
+                    DockInfo = new DockInfo { Header = plugin.Name, },
+                    DockMode = DockMode.Side,
+                    Dock = Dock.Top,
+                });
+        });
 
         OpenSettings = ReactiveCommand.Create(() => {
             var settingsWindow = new SettingsWindow(componentContext.Resolve<ISettingsVM>());
