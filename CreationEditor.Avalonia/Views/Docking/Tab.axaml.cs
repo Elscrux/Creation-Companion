c87468c5b700aaf16ec6720bd51dc4d899e03b04
nickp <nickposer2102@gmail.com>
2023-04-25 20:00:17 +0200

    Separated DockTab for to avoid code duplicates

---
+++ CreationEditor.Avalonia/Views/Docking/Tab.axaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using CreationEditor.Avalonia.Models.Docking;
+using CreationEditor.Avalonia.ViewModels.Docking;
+namespace CreationEditor.Avalonia.Views.Docking;
+
+public partial class Tab : UserControl {
+    public static readonly StyledProperty<IDockedItem> DockedItemProperty
+        = AvaloniaProperty.Register<Tab, IDockedItem>(nameof(DockedItem));
+
+    public IDockedItem DockedItem {
+        get => GetValue(DockedItemProperty);
+        set => SetValue(DockedItemProperty, value);
+    }
+
+    public static readonly StyledProperty<TabbedDockVM?> DockContainerProperty
+        = AvaloniaProperty.Register<Tab, TabbedDockVM?>(nameof(DockContainer));
+
+    public TabbedDockVM? DockContainer {
+        get => GetValue(DockContainerProperty);
+        set => SetValue(DockContainerProperty, value);
+    }
+
+    public Tab() {
+        InitializeComponent();
+    }
+}
