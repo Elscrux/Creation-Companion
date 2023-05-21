5cc2da4a7a26f31fc0a7664d872a651f1725fc35
nickp <nickposer2102@gmail.com>
2023-05-21 22:00:31 +0200

    Add keyed observable collection with selector

--- CreationEditor/Extension/ObservableCollectionSelectorAdaptor.cs
+++ CreationEditor/Extension/ObservableCollectionSelectorAdaptor.cs
@@ -29,7 +29,7 @@ public sealed class ObservableCollectionSelectorAdaptor<T, TTarget> {
     /// </summary>
     /// <param name="changes">The changes.</param>
     public IChangeSet<TTarget> Adapt(IChangeSet<T> changes) {
-        if (changes is null) throw new ArgumentNullException(nameof(changes));
+        ArgumentNullException.ThrowIfNull(changes);
 
         var newChanges = new ChangeSet<TTarget>();
         foreach (var change in changes) {
@@ -51,6 +51,70 @@ public sealed class ObservableCollectionSelectorAdaptor<T, TTarget> {
                 default:
                     throw new ArgumentOutOfRangeException();
             }
+        }
+
+        if (newChanges.TotalChanges - newChanges.Refreshes > _refreshThreshold || !_loaded) {
+            using (_collection.SuspendNotifications()) {
+                _collection.Clone(newChanges);
+                _loaded = true;
+            }
+        } else {
+            _collection.Clone(newChanges);
+        }
+
+        return newChanges;
+    }
+}
+
+public sealed class ObservableCollectionSelectorAdaptor<TObj, TKey, TTarget> where TKey : notnull {
+    private readonly Func<TObj, TKey, TTarget> _selector;
+    private readonly IObservableCollection<TTarget> _collection;
+
+    private readonly int _refreshThreshold;
+
+    private bool _loaded;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ObservableCollectionAdaptor{T}"/> class.
+    /// </summary>
+    /// <param name="selector"></param>
+    /// <param name="collection">The collection.</param>
+    /// <param name="refreshThreshold">The refresh threshold.</param>
+    /// <exception cref="System.ArgumentNullException">collection.</exception>
+    public ObservableCollectionSelectorAdaptor(Func<TObj, TKey, TTarget> selector, IObservableCollection<TTarget> collection, int refreshThreshold = 25) {
+        _selector = selector;
+        _collection = collection ?? throw new ArgumentNullException(nameof(collection));
+        _refreshThreshold = refreshThreshold;
+    }
+
+    /// <summary>
+    /// Maintains the specified collection from the changes.
+    /// </summary>
+    /// <param name="changes">The changes.</param>
+    public IChangeSet<TTarget> Adapt(IChangeSet<TObj, TKey> changes) {
+        ArgumentNullException.ThrowIfNull(changes);
+
+        var newChanges = new ChangeSet<TTarget>();
+        foreach (var change in changes) {
+            var previous = change.Previous.HasValue
+                ? Optional<TTarget>.Create(_selector(change.Previous.Value, change.Key))
+                : Optional<TTarget>.None;
+
+            var current = _selector(change.Current, change.Key);
+
+            newChanges.Add(new Change<TTarget>(
+                change.Reason switch {
+                    ChangeReason.Add => ListChangeReason.Add,
+                    ChangeReason.Update => ListChangeReason.Replace,
+                    ChangeReason.Remove => ListChangeReason.Remove,
+                    ChangeReason.Refresh => ListChangeReason.Refresh,
+                    ChangeReason.Moved => ListChangeReason.Moved,
+                    _ => throw new ArgumentOutOfRangeException()
+                },
+                current,
+                previous,
+                change.CurrentIndex,
+                change.PreviousIndex));
         }
 
         if (newChanges.TotalChanges - newChanges.Refreshes > _refreshThreshold || !_loaded) {
--- CreationEditor/Extension/ObservableExtension.cs
+++ CreationEditor/Extension/ObservableExtension.cs
@@ -54,6 +54,19 @@ public static class ObservableExtension {
         return destination;
     }
 
+    public static ReadOnlyObservableCollection<TTarget> ToObservableCollection<TObj, TKey, TTarget>(
+        this IObservable<ISortedChangeSet<TObj, TKey>> changeSet,
+        Func<TObj, TKey, TTarget> selector,
+        IDisposableDropoff disposable) where TKey : notnull {
+        changeSet
+            .ObserveOnGui()
+            .Bind(selector, out var readOnlyObservableCollection)
+            .Subscribe()
+            .DisposeWith(disposable);
+
+        return readOnlyObservableCollection;
+    }
+
     public static ReadOnlyObservableCollection<TObj> ToObservableCollection<TObj>(
         this IObservable<IChangeSet<TObj>> changeSet,
         IDisposableDropoff disposable) {
@@ -179,6 +192,31 @@ public static class ObservableExtension {
     }
 
     /// <summary>
+    /// Creates a binding to a readonly observable collection which is specified as an 'out' parameter.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TObj"></typeparam>
+    /// <typeparam name="TTarget"></typeparam>
+    /// <param name="source">The source.</param>
+    /// <param name="selector"></param>
+    /// <param name="readOnlyObservableCollection">The resulting read only observable collection.</param>
+    /// <param name="resetThreshold">The reset threshold.</param>
+    /// <returns>A continuation of the source stream.</returns>
+    public static IObservable<IChangeSet<TTarget>> Bind<TObj, TKey, TTarget>(
+        this IObservable<IChangeSet<TObj, TKey>> source,
+        Func<TObj, TKey, TTarget> selector,
+        out ReadOnlyObservableCollection<TTarget> readOnlyObservableCollection,
+        int resetThreshold = 25) where TKey : notnull {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var target = new ObservableCollectionExtended<TTarget>();
+        var result = new ReadOnlyObservableCollection<TTarget>(target);
+        var adaptor = new ObservableCollectionSelectorAdaptor<TObj, TKey, TTarget>(selector, target, resetThreshold);
+        readOnlyObservableCollection = result;
+        return source.Adapt(adaptor);
+    }
+
+    /// <summary>
     /// Injects a side effect into a change set observable.
     /// </summary>
     /// <typeparam name="T">The type of the item.</typeparam>
@@ -194,6 +232,35 @@ public static class ObservableExtension {
     public static IObservable<IChangeSet<TTarget>> Adapt<T, TTarget>(
         this IObservable<IChangeSet<T>> source,
         ObservableCollectionSelectorAdaptor<T, TTarget> adaptor) {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(adaptor);
+
+        return Observable.Create<IChangeSet<TTarget>>(
+            observer => {
+                var locker = new object();
+                return source.Synchronize(locker)
+                    .Select(adaptor.Adapt)
+                    .SubscribeSafe(observer);
+            });
+    }
+
+    /// <summary>
+    /// Injects a side effect into a change set observable.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TObj"></typeparam>
+    /// <typeparam name="TTarget"></typeparam>
+    /// <param name="source">The source.</param>
+    /// <param name="adaptor">The adaptor.</param>
+    /// <returns>An observable which emits the change set.</returns>
+    /// <exception cref="System.ArgumentNullException">
+    /// source
+    /// or
+    /// adaptor.
+    /// </exception>
+    public static IObservable<IChangeSet<TTarget>> Adapt<TObj, TKey, TTarget>(
+        this IObservable<IChangeSet<TObj, TKey>> source,
+        ObservableCollectionSelectorAdaptor<TObj, TKey, TTarget> adaptor) where TKey : notnull {
         ArgumentNullException.ThrowIfNull(source);
         ArgumentNullException.ThrowIfNull(adaptor);
 
