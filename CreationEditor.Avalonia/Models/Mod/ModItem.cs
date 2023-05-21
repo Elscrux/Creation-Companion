fb66a78489473d6cb6dbf15fc2dd6ced5dee0e2a
nickp <nickposer2102@gmail.com>
2023-05-21 11:55:34 +0200

    Remove unnecessary new plugin identifier

--- CreationEditor.Avalonia/Models/Mod/ModItem.cs
+++ CreationEditor.Avalonia/Models/Mod/ModItem.cs
@@ -5,13 +5,8 @@ using ReactiveUI.Fody.Helpers;
 namespace CreationEditor.Avalonia.Models.Mod;
 
 public class ModItem : ReactiveObject, IReactiveSelectable {
-    public const string NewPluginIdentifier = "< new plugin >";
-    public static readonly ModKey NewMod = new();
-
     public ModItem(ModKey modKey) => ModKey = modKey;
 
     [Reactive] public bool IsSelected { get; set; }
     public ModKey ModKey { get; }
-
-    public override string ToString() => ModKey == NewMod ? NewPluginIdentifier : ModKey.FileName;
 }
