c7d83d79736b6a9e00a29bf684c0cf74ac1fa88a
nickp <nickposer2102@gmail.com>
2023-06-05 21:45:56 +0200

    Make collection items nullable

--- CreationEditor/Extension/NotifyCollectionChangedEventArgsExtension.cs
+++ CreationEditor/Extension/NotifyCollectionChangedEventArgsExtension.cs
@@ -4,28 +4,28 @@ namespace CreationEditor;
 public static class NotifyCollectionChangedEventArgsExtension {
     public static NotifyCollectionChangedEventArgs Transform<TSource, TTarget>(
         this NotifyCollectionChangedEventArgs e,
-        Func<TSource, TTarget> selector) {
+        Func<TSource, TTarget?> selector) {
         return (e.Action) switch {
             NotifyCollectionChangedAction.Add =>
                 new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Add,
-                    e.NewItems?.OfType<TSource>().Select(selector).ToList(),
+                    e.NewItems?.OfType<TSource>().Select(selector).Where(x => x is not null).ToList(),
                     e.NewStartingIndex),
             NotifyCollectionChangedAction.Remove =>
                 new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Remove,
-                    e.OldItems?.OfType<TSource>().Select(selector).ToList(),
+                    e.OldItems?.OfType<TSource>().Select(selector).Where(x => x is not null).ToList(),
                     e.OldStartingIndex),
             NotifyCollectionChangedAction.Replace =>
                 new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Replace,
-                    e.NewItems?.OfType<TSource>().Select(selector).ToList(),
+                    e.NewItems?.OfType<TSource>().Select(selector).Where(x => x is not null).ToList(),
                     e.NewStartingIndex,
                     e.OldStartingIndex),
             NotifyCollectionChangedAction.Move =>
                 new NotifyCollectionChangedEventArgs(
                     NotifyCollectionChangedAction.Move,
-                    e.NewItems?.OfType<TSource>().Select(selector).ToList(),
+                    e.NewItems?.OfType<TSource>().Select(selector).Where(x => x is not null).ToList(),
                     e.NewStartingIndex,
                     e.OldStartingIndex),
             NotifyCollectionChangedAction.Reset =>
