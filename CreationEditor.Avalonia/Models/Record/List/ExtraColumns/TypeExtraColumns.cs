fe1991b3ae24eaf01b77946f93092dc2c480cec2
nickp <nickposer2102@gmail.com>
2023-06-05 21:49:06 +0200

    Use refactorable binding

--- CreationEditor.Avalonia/Models/Record/List/ExtraColumns/TypeExtraColumns.cs
+++ CreationEditor.Avalonia/Models/Record/List/ExtraColumns/TypeExtraColumns.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Data;
 using CreationEditor.Avalonia.Comparer;
+using CreationEditor.Services.Mutagen.References.Record;
 namespace CreationEditor.Avalonia.Models.Record.List.ExtraColumns;
 
 public class TypeExtraColumns : IUntypedExtraColumns {
@@ -9,7 +10,7 @@ public class TypeExtraColumns : IUntypedExtraColumns {
             yield return new ExtraColumn(
                 new DataGridTextColumn {
                     Header = "Type",
-                    Binding = new Binding("RecordTypeName", BindingMode.OneWay),
+                    Binding = new Binding(nameof(IReferencedRecord.RecordTypeName), BindingMode.OneWay),
                     CanUserSort = true,
                     CustomSortComparer = ReferencedRecordComparers.TypeComparer,
                 },
