57ebc57cadb90d8f92eb2db5ebe5fa7b301f9796
nickp <nickposer2102@gmail.com>
2023-06-20 22:33:28 +0200

    Add logging info

--- CreationEditor.Avalonia/Services/Viewport/ViewportFactory.cs
+++ CreationEditor.Avalonia/Services/Viewport/ViewportFactory.cs
@@ -2,17 +2,21 @@
 using Avalonia.Controls;
 using CreationEditor.Avalonia.Views.Viewport;
 using Mutagen.Bethesda.Environments.DI;
+using Serilog;
 namespace CreationEditor.Avalonia.Services.Viewport;
 
 public sealed class BSEViewportFactory : IViewportFactory {
+    private readonly ILogger _logger;
     private readonly IDataDirectoryProvider _dataDirectoryProvider;
     private readonly IFileSystem _fileSystem;
 
     public bool IsMultiInstanceCapable => false;
 
     public BSEViewportFactory(
+        ILogger logger,
         IDataDirectoryProvider dataDirectoryProvider,
         IFileSystem fileSystem) {
+        _logger = logger;
         _dataDirectoryProvider = dataDirectoryProvider;
         _fileSystem = fileSystem;
     }
@@ -24,6 +28,15 @@ public sealed class BSEViewportFactory : IViewportFactory {
             .Select(path => _fileSystem.Path.GetFileName(path))
             // .OrderBy() todo order by load order priority (+ exclude non-loaded BSAs? would need ini parsing to check which inis are always loaded)
             .ToArray();
+
+        _logger.Here().Information("Initializing viewport with data directory {AssetDirectory}", assetDirectory);
+
+        if (bsaFileNames.Length > 0) {
+            _logger.Here().Debug(
+                "Loading BSAs: {FileNames}", string.Join(", ", bsaFileNames));
+        } else {
+            _logger.Here().Warning("No BSAs detected in {AssetDirectory}", assetDirectory);
+        }
 
         return new ViewportBSE(
             assetDirectory,
