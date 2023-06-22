cbc4d9f311b4d67cd8cab6061f51df644106f23a
nickp <nickposer2102@gmail.com>
2023-06-23 00:14:09 +0200

    Add wip record selection

---
+++ CreationEditor.Avalonia/ViewModels/Record/List/RecordSelectionVM.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Reactive.Linq;
+using Avalonia;
+using CreationEditor.Avalonia.ViewModels.Record.Provider;
+using CreationEditor.Services.Mutagen.References.Record;
+using DynamicData;
+using Mutagen.Bethesda.Plugins;
+using Noggog;
+using ReactiveUI;
+namespace CreationEditor.Avalonia.ViewModels.Record.List;
+
+public sealed class RecordSelectionVM : ViewModel {
+    public IRecordProvider RecordProvider { get; }
+
+    public IEnumerable? Records { get; }
+
+    public IObservable<bool> IsBusy { get; }
+
+    public Func<StyledElement, IFormLinkIdentifier> GetFormLink { get; }
+
+    public RecordSelectionVM(
+        IRecordProvider recordProvider) {
+        RecordProvider = recordProvider.DisposeWith(this);
+
+        Records = RecordProvider.RecordCache
+            .Connect()
+            .SubscribeOn(RxApp.TaskpoolScheduler)
+            .WrapInInProgressMarker(
+                x => x.Filter(RecordProvider.Filter, false),
+                out var isFiltering)
+            .ToObservableCollection(this);
+
+        IsBusy = isFiltering
+            .CombineLatest(
+                RecordProvider.IsBusy,
+                (filtering, busy) => (Filtering: filtering, Busy: busy))
+            .ObserveOnGui()
+            .Select(list => list.Filtering || list.Busy);
+
+        GetFormLink = element => {
+            if (element.DataContext is not IReferencedRecord referencedRecord) return FormLinkInformation.Null;
+
+            return FormLinkInformation.Factory(referencedRecord.Record);
+        };
+    }
+}
---
+++ CreationEditor.Avalonia/Views/Record/RecordSelection.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.ReactiveUI;
+using CreationEditor.Avalonia.ViewModels.Record.List;
+namespace CreationEditor.Avalonia.Views.Record;
+
+public partial class RecordSelection : ReactiveUserControl<RecordSelectionVM> {
+    public RecordSelection() {
+        InitializeComponent();
+    }
+
+    public RecordSelection(RecordSelectionVM vm) : this() {
+        DataContext = vm;
+    }
+}
