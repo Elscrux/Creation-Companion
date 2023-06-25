44cbd64ac13d96125a3178e513093bdef9a29b81
nickp <nickposer2102@gmail.com>
2023-06-25 22:33:10 +0200

    Add condition utility functions

---
+++ CreationEditor.Skyrim/Extension/MajorRecordExtension.cs
@@ -0,0 +1,252 @@
+﻿using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+namespace CreationEditor.Skyrim;
+
+public static class MajorRecordExtension {
+    public static IEnumerable<ExtendedList<Condition>> GetConditions(this IMajorRecord record) {
+        switch (record) {
+            case IQuest quest:
+                yield return quest.DialogConditions;
+                yield return quest.EventConditions;
+
+                foreach (var objective in quest.Objectives) {
+                    foreach (var target in objective.Targets) {
+                        yield return target.Conditions;
+                    }
+                }
+
+                foreach (var stage in quest.Stages) {
+                    foreach (var logEntry in stage.LogEntries) {
+                        yield return logEntry.Conditions;
+                    }
+                }
+
+                foreach (var alias in quest.Aliases) {
+                    yield return alias.Conditions;
+                }
+                break;
+            case IDialogResponses responses:
+                yield return responses.Conditions;
+
+                break;
+            case ILoadScreen loadScreen:
+                yield return loadScreen.Conditions;
+
+                break;
+
+            case IMessage message:
+                foreach (var menuButton in message.MenuButtons) {
+                    yield return menuButton.Conditions;
+                }
+                break;
+            case IPerk perk:
+                yield return perk.Conditions;
+
+                foreach (var effect in perk.Effects) {
+                    foreach (var perkCondition in effect.Conditions) {
+                        yield return perkCondition.Conditions;
+                    }
+                }
+                break;
+            case IMusicTrack musicTrack:
+                if (musicTrack.Conditions != null) yield return musicTrack.Conditions;
+
+                break;
+            case ISoundDescriptor soundDescriptor:
+                yield return soundDescriptor.Conditions;
+
+                break;
+            case IFaction faction:
+                if (faction.Conditions != null) yield return faction.Conditions;
+
+                break;
+            case IPackage package:
+                yield return package.Conditions;
+
+                break;
+            case IScene scene:
+                yield return scene.Conditions;
+
+                foreach (var phase in scene.Phases) {
+                    yield return phase.StartConditions;
+                    yield return phase.CompletionConditions;
+                }
+                break;
+            case IStoryManagerBranchNode storyManagerBranchNode:
+                yield return storyManagerBranchNode.Conditions;
+
+                break;
+            case IStoryManagerEventNode storyManagerEventNode:
+                yield return storyManagerEventNode.Conditions;
+
+                break;
+            case IStoryManagerQuestNode storyManagerQuestNode:
+                yield return storyManagerQuestNode.Conditions;
+
+                break;
+            case IConstructibleObject constructibleObject:
+                yield return constructibleObject.Conditions;
+
+                break;
+            case IIdleAnimation idleAnimation:
+                yield return idleAnimation.Conditions;
+
+                break;
+            case IIngredient ingredient:
+                foreach (var effect in ingredient.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IIngestible ingestible:
+                foreach (var effect in ingestible.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IObjectEffect objectEffect:
+                foreach (var effect in objectEffect.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IScroll scroll:
+                foreach (var effect in scroll.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case ISpell spell:
+                foreach (var effect in spell.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IMagicEffect magicEffect:
+                yield return magicEffect.Conditions;
+
+                break;
+            default:
+                yield break;
+        }
+    }
+
+    public static IEnumerable<IReadOnlyList<IConditionGetter>> GetConditions(this IMajorRecordGetter record) {
+        switch (record) {
+            case IQuestGetter quest:
+                yield return quest.DialogConditions;
+                yield return quest.EventConditions;
+
+                foreach (var objective in quest.Objectives) {
+                    foreach (var target in objective.Targets) {
+                        yield return target.Conditions;
+                    }
+                }
+
+                foreach (var stage in quest.Stages) {
+                    foreach (var logEntry in stage.LogEntries) {
+                        yield return logEntry.Conditions;
+                    }
+                }
+
+                foreach (var alias in quest.Aliases) {
+                    yield return alias.Conditions;
+                }
+                break;
+            case IDialogResponsesGetter responses:
+                yield return responses.Conditions;
+
+                break;
+            case ILoadScreenGetter loadScreen:
+                yield return loadScreen.Conditions;
+
+                break;
+
+            case IMessageGetter message:
+                foreach (var menuButton in message.MenuButtons) {
+                    yield return menuButton.Conditions;
+                }
+                break;
+            case IPerkGetter perk:
+                yield return perk.Conditions;
+
+                foreach (var effect in perk.Effects) {
+                    foreach (var perkCondition in effect.Conditions) {
+                        yield return perkCondition.Conditions;
+                    }
+                }
+                break;
+            case IMusicTrackGetter musicTrack:
+                if (musicTrack.Conditions != null) yield return musicTrack.Conditions;
+
+                break;
+            case ISoundDescriptorGetter soundDescriptor:
+                yield return soundDescriptor.Conditions;
+
+                break;
+            case IFactionGetter faction:
+                if (faction.Conditions != null) yield return faction.Conditions;
+
+                break;
+            case IPackageGetter package:
+                yield return package.Conditions;
+
+                break;
+            case ISceneGetter scene:
+                yield return scene.Conditions;
+
+                foreach (var phase in scene.Phases) {
+                    yield return phase.StartConditions;
+                    yield return phase.CompletionConditions;
+                }
+                break;
+            case IStoryManagerBranchNodeGetter storyManagerBranchNode:
+                yield return storyManagerBranchNode.Conditions;
+
+                break;
+            case IStoryManagerEventNodeGetter storyManagerEventNode:
+                yield return storyManagerEventNode.Conditions;
+
+                break;
+            case IStoryManagerQuestNodeGetter storyManagerQuestNode:
+                yield return storyManagerQuestNode.Conditions;
+
+                break;
+            case IConstructibleObjectGetter constructibleObject:
+                yield return constructibleObject.Conditions;
+
+                break;
+            case IIdleAnimationGetter idleAnimation:
+                yield return idleAnimation.Conditions;
+
+                break;
+            case IIngredientGetter ingredient:
+                foreach (var effect in ingredient.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IIngestibleGetter ingestible:
+                foreach (var effect in ingestible.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IObjectEffectGetter objectEffect:
+                foreach (var effect in objectEffect.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IScrollGetter scroll:
+                foreach (var effect in scroll.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case ISpellGetter spell:
+                foreach (var effect in spell.Effects) {
+                    yield return effect.Conditions;
+                }
+                break;
+            case IMagicEffectGetter magicEffect:
+                yield return magicEffect.Conditions;
+
+                break;
+            default:
+                yield break;
+        }
+    }
+}
--- CreationEditor.Skyrim/Extension/ModExtension.cs
+++ CreationEditor.Skyrim/Extension/ModExtension.cs
@@ -9,4 +9,130 @@ public static class ModExtension {
             _ => throw new ArgumentOutOfRangeException(nameof(mod))
         };
     }
+
+	public static IEnumerable<IReadOnlyList<IConditionGetter>> GetConditions(this IModGetter mod) {
+		foreach (var quest in mod.EnumerateMajorRecords<IQuestGetter>()) {
+			yield return quest.DialogConditions;
+			yield return quest.EventConditions;
+
+			foreach (var objective in quest.Objectives) {
+				foreach (var target in objective.Targets) {
+					yield return target.Conditions;
+				}
+			}
+
+			foreach (var stage in quest.Stages) {
+				foreach (var logEntry in stage.LogEntries) {
+					yield return logEntry.Conditions;
+				}
+			}
+
+			foreach (var alias in quest.Aliases) {
+				yield return alias.Conditions;
+			}
+		}
+
+		foreach (var responses in mod.EnumerateMajorRecords<IDialogResponsesGetter>()) {
+			yield return responses.Conditions;
+		}
+
+		foreach (var loadScreen in mod.EnumerateMajorRecords<ILoadScreenGetter>()) {
+			yield return loadScreen.Conditions;
+		}
+
+		foreach (var message in mod.EnumerateMajorRecords<IMessageGetter>()) {
+			foreach (var menuButton in message.MenuButtons) {
+				yield return menuButton.Conditions;
+			}
+		}
+
+		foreach (var perk in mod.EnumerateMajorRecords<IPerkGetter>()) {
+			yield return perk.Conditions;
+
+			foreach (var effect in perk.Effects) {
+				foreach (var perkCondition in effect.Conditions) {
+					yield return perkCondition.Conditions;
+				}
+			}
+		}
+
+		foreach (var musicTrack in mod.EnumerateMajorRecords<IMusicTrackGetter>()) {
+			if (musicTrack.Conditions != null) yield return musicTrack.Conditions;
+		}
+
+		foreach (var soundDescriptor in mod.EnumerateMajorRecords<ISoundDescriptorGetter>()) {
+			yield return soundDescriptor.Conditions;
+		}
+
+		foreach (var faction in mod.EnumerateMajorRecords<IFactionGetter>()) {
+			if (faction.Conditions != null) yield return faction.Conditions;
+		}
+
+		foreach (var package in mod.EnumerateMajorRecords<IPackageGetter>()) {
+			yield return package.Conditions;
+		}
+
+		foreach (var scene in mod.EnumerateMajorRecords<ISceneGetter>()) {
+			yield return scene.Conditions;
+
+			foreach (var phase in scene.Phases) {
+				yield return phase.StartConditions;
+				yield return phase.CompletionConditions;
+			}
+		}
+
+		foreach (var storyManagerBranchNode in mod.EnumerateMajorRecords<IStoryManagerBranchNodeGetter>()) {
+			yield return storyManagerBranchNode.Conditions;
+		}
+
+		foreach (var storyManagerEventNode in mod.EnumerateMajorRecords<IStoryManagerEventNodeGetter>()) {
+			yield return storyManagerEventNode.Conditions;
+		}
+
+		foreach (var storyManagerQuestNode in mod.EnumerateMajorRecords<IStoryManagerQuestNodeGetter>()) {
+			yield return storyManagerQuestNode.Conditions;
+		}
+
+		foreach (var constructibleObject in mod.EnumerateMajorRecords<IConstructibleObjectGetter>()) {
+			yield return constructibleObject.Conditions;
+		}
+
+		foreach (var idleAnimation in mod.EnumerateMajorRecords<IIdleAnimationGetter>()) {
+			yield return idleAnimation.Conditions;
+		}
+
+		foreach (var ingredient in mod.EnumerateMajorRecords<IIngredientGetter>()) {
+			foreach (var effect in ingredient.Effects) {
+				yield return effect.Conditions;
+			}
+		}
+
+		foreach (var ingestible in mod.EnumerateMajorRecords<IIngestibleGetter>()) {
+			foreach (var effect in ingestible.Effects) {
+				yield return effect.Conditions;
+			}
+		}
+
+		foreach (var objectEffect in mod.EnumerateMajorRecords<IObjectEffectGetter>()) {
+			foreach (var effect in objectEffect.Effects) {
+				yield return effect.Conditions;
+			}
+		}
+
+		foreach (var scroll in mod.EnumerateMajorRecords<IScrollGetter>()) {
+			foreach (var effect in scroll.Effects) {
+				yield return effect.Conditions;
+			}
+		}
+
+		foreach (var spell in mod.EnumerateMajorRecords<ISpellGetter>()) {
+			foreach (var effect in spell.Effects) {
+				yield return effect.Conditions;
+			}
+		}
+
+		foreach (var magicEffect in mod.EnumerateMajorRecords<IMagicEffectGetter>()) {
+			yield return magicEffect.Conditions;
+		}
+	}
 }
