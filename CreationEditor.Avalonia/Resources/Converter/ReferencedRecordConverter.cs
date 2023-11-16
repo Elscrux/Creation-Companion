2967d539d8e2afd6512a50a2e1769482b02e3813
nickp <nickposer2102@gmail.com>
2023-11-16 15:45:31 +0100

    Move ReferencedRecordConverter to proper place

--- CreationEditor.Avalonia/Resources/Converter/ReferencedRecordConverter.cs
+++ CreationEditor.Avalonia/Resources/Converter/ReferencedRecordConverter.cs
@@ -1,7 +1,7 @@
 ﻿using Avalonia.Data.Converters;
 using CreationEditor.Services.Mutagen.References.Record;
 using Mutagen.Bethesda.Plugins.Records;
-namespace CreationEditor.Avalonia;
+namespace CreationEditor.Avalonia.Converter;
 
 public static class ReferencedRecordConverter {
     public static FuncValueConverter<IReferencedRecord, IMajorRecordGetter?> ToRecord { get; }
