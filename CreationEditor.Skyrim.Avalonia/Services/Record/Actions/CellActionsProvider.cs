af0fd7f9a8191bc6b4bd27916e2bd07ee4c88791
nickp <nickposer2102@gmail.com>
2024-01-27 22:26:44 +0100

    Add missing using

--- CreationEditor.Skyrim.Avalonia/Services/Record/Actions/CellActionsProvider.cs
+++ CreationEditor.Skyrim.Avalonia/Services/Record/Actions/CellActionsProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CreationEditor.Avalonia.Models;
 using CreationEditor.Avalonia.Services;
 using CreationEditor.Avalonia.Services.Avalonia;
