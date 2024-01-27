ec2309f67caa711ff8200bce11ff77fd1536d025
nickp <nickposer2102@gmail.com>
2024-01-27 17:19:08 +0100

    Add utility function

---
+++ CreationEditor/Extension/EnumerableExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace CreationEditor;
+
+public static class EnumerableExtension {
+    public static bool CountIsExactly<T>(this IEnumerable<T> enumerable, int count) {
+        var counter = 0;
+        foreach (var _ in enumerable) {
+            counter++;
+            if (counter > count) return false;
+        }
+
+        return counter == count;
+    }
+}
