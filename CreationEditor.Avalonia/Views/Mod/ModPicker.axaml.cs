09d325d8103a3c15862742e25e7750412e0e1852
nickp <nickposer2102@gmail.com>
2024-01-28 17:46:59 +0100

    Fix xaml keybindings and flyout overriding selection keybindings and flyout options

--- CreationEditor.Avalonia/Views/Mod/ModPicker.axaml.cs
+++ CreationEditor.Avalonia/Views/Mod/ModPicker.axaml.cs
@@ -1,8 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.ReactiveUI;
 using CreationEditor.Avalonia.ViewModels.Mod;
 namespace CreationEditor.Avalonia.Views.Mod;
 
-public partial class ModPicker : UserControl {
+public partial class ModPicker : ReactiveUserControl<ModPickerVM> {
     public ModPicker() {
         InitializeComponent();
     }
