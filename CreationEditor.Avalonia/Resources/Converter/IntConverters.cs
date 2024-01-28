45eaf017f5069b6bed6e4dcbb3195b572e0d2f21
nickp <nickposer2102@gmail.com>
2024-01-28 17:45:18 +0100

    Add int converters

---
+++ CreationEditor.Avalonia/Resources/Converter/IntConverters.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Data.Converters;
+namespace CreationEditor.Avalonia.Converter;
+
+public static class IntConverters {
+    public new static readonly ExtendedFuncValueConverter<int, bool, int> Equals
+        = new((x, y) => x == y);
+
+    public static readonly ExtendedFuncValueConverter<int, bool, int> NotEquals
+        = new((x, y) => x != y);
+
+    public static readonly FuncMultiValueConverter<int, int> Min
+        = new(ints => ints.Min());
+
+    public static readonly FuncMultiValueConverter<int, int> Max
+        = new(ints => ints.Max());
+}
