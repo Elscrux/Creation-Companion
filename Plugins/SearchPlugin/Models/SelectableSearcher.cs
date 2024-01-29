504723300c5de9d2908de9c5d8c600ea43f81bc4
nickp <nickposer2102@gmail.com>
2024-01-29 19:15:59 +0100

    Use primary constructor

--- Plugins/SearchPlugin/Models/SelectableSearcher.cs
+++ Plugins/SearchPlugin/Models/SelectableSearcher.cs
@@ -3,11 +3,7 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 namespace SearchPlugin.Models;
 
-public sealed class SelectableSearcher : ReactiveObject, IReactiveSelectable {
-    public ITextSearcherDefinition Searcher { get; }
+public sealed class SelectableSearcher(ITextSearcherDefinition searcher) : ReactiveObject, IReactiveSelectable {
+    public ITextSearcherDefinition Searcher { get; } = searcher;
     [Reactive] public bool IsSelected { get; set; } = true;
-
-    public SelectableSearcher(ITextSearcherDefinition searcher) {
-        Searcher = searcher;
-    }
 }
