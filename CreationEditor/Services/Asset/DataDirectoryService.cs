b527a7ab10b9df0bf5d5c35993184adb2316a3a3
Elscrux <nickposer2102@gmail.com>
2024-04-24 23:31:36 +0200

    Refactor data directory service

--- CreationEditor/Services/Asset/DataDirectoryService.cs
+++ CreationEditor/Services/Asset/DataDirectoryService.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO.Abstractions;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using Mutagen.Bethesda.Environments.DI;
 using Noggog;
 using ReactiveMarbles.ObservableEvents;
@@ -9,14 +10,34 @@ public sealed class DataDirectoryService : IDataDirectoryService {
     private readonly DisposableBucket _disposables = new();
 
     private readonly IAssetTypeService _assetTypeService;
+    private readonly IDeleteDirectoryProvider _deleteDirectoryProvider;
     private readonly IFileSystem _fileSystem;
 
     public string Path { get; }
 
-    public IObservable<FileSystemEventArgs> Created { get; }
-    public IObservable<FileSystemEventArgs> Deleted { get; }
-    public IObservable<FileSystemEventArgs> Changed { get; }
-    public IObservable<RenamedEventArgs> Renamed { get; }
+    public IObservable<FileSystemEventArgs> Created => _createdFile.Merge(_createdDirectory);
+    public IObservable<FileSystemEventArgs> CreatedFile => _createdFile;
+    public IObservable<FileSystemEventArgs> CreatedDirectory => _createdDirectory;
+    private readonly Subject<FileSystemEventArgs> _createdFile = new();
+    private readonly Subject<FileSystemEventArgs> _createdDirectory = new();
+
+    public IObservable<FileSystemEventArgs> Deleted => _deletedFile.Merge(_deletedDirectory);
+    public IObservable<FileSystemEventArgs> DeletedFile => _deletedFile;
+    public IObservable<FileSystemEventArgs> DeletedDirectory => _deletedDirectory;
+    private readonly Subject<FileSystemEventArgs> _deletedFile = new();
+    private readonly Subject<FileSystemEventArgs> _deletedDirectory = new();
+
+    public IObservable<FileSystemEventArgs> Changed => _changedFile.Merge(_changedDirectory);
+    public IObservable<FileSystemEventArgs> ChangedFile => _changedFile;
+    public IObservable<FileSystemEventArgs> ChangedDirectory => _changedDirectory;
+    private readonly Subject<FileSystemEventArgs> _changedFile = new();
+    private readonly Subject<FileSystemEventArgs> _changedDirectory = new();
+
+    public IObservable<RenamedEventArgs> Renamed => _renamedFile.Merge(_renamedDirectory);
+    public IObservable<RenamedEventArgs> RenamedFile => _renamedFile;
+    public IObservable<RenamedEventArgs> RenamedDirectory => _renamedDirectory;
+    private readonly Subject<RenamedEventArgs> _renamedFile = new();
+    private readonly Subject<RenamedEventArgs> _renamedDirectory = new();
 
     public DataDirectoryService(
         IDataDirectoryProvider dataDirectoryProvider,
@@ -24,7 +45,7 @@ public sealed class DataDirectoryService : IDataDirectoryService {
         IFileSystem fileSystem,
         IAssetTypeService assetTypeService) {
         Path = dataDirectoryProvider.Path;
-        var deleteDirectoryProvider1 = deleteDirectoryProvider;
+        _deleteDirectoryProvider = deleteDirectoryProvider;
         _fileSystem = fileSystem;
         _assetTypeService = assetTypeService;
         var watcher = _fileSystem.FileSystemWatcher.New(Path).DisposeWith(_disposables);
@@ -32,31 +53,35 @@ public sealed class DataDirectoryService : IDataDirectoryService {
         watcher.IncludeSubdirectories = true;
         watcher.EnableRaisingEvents = true;
 
-        Created = watcher.Events().Created
-            .Where(e =>
-                (!string.Equals(e.FullPath, deleteDirectoryProvider1.DeleteDirectory, AssetCompare.PathComparison)
-                 && !_fileSystem.Path.HasExtension(e.FullPath)) || IsFileRelevant(e.FullPath));
-
-        Deleted = watcher.Events().Deleted
-            .Where(e =>
-                (!string.Equals(e.FullPath, deleteDirectoryProvider1.DeleteDirectory, AssetCompare.PathComparison)
-                 && !_fileSystem.Path.HasExtension(e.FullPath)) || IsFileRelevant(e.FullPath));
-
-        Changed = watcher.Events().Changed
-            .Where(e =>
-                (!string.Equals(e.FullPath, deleteDirectoryProvider1.DeleteDirectory, AssetCompare.PathComparison)
-                 && !_fileSystem.Path.HasExtension(e.FullPath)) || IsFileRelevant(e.FullPath));
-
-        Renamed = watcher.Events().Renamed
-            .Where(e =>
-                (!string.Equals(e.FullPath, deleteDirectoryProvider1.DeleteDirectory, AssetCompare.PathComparison)
-                 && !_fileSystem.Path.HasExtension(e.FullPath)) || IsFileRelevant(e.FullPath));
+        var fileSystemWatcherEvents = watcher.Events();
+        HandleFileSystemEvent(fileSystemWatcherEvents.Created, _createdFile, _createdDirectory);
+        HandleFileSystemEvent(fileSystemWatcherEvents.Deleted, _deletedFile, _deletedDirectory);
+        HandleFileSystemEvent(fileSystemWatcherEvents.Changed, _changedFile, _changedDirectory);
+        HandleFileSystemEvent(fileSystemWatcherEvents.Renamed, _renamedFile, _renamedDirectory);
     }
 
-    private bool IsFileRelevant(string file) {
-        var extension = _fileSystem.Path.GetExtension(file);
+    private void HandleFileSystemEvent<T>(IObservable<T> observable, IObserver<T> fileObserver, IObserver<T> directoryObserver)
+        where T : FileSystemEventArgs {
+        observable
+            .Where(Filter)
+            .Subscribe(x => {
+                if (_fileSystem.Directory.Exists(x.FullPath)) {
+                    directoryObserver.OnNext(x);
+                } else {
+                    var extension = _fileSystem.Path.GetExtension(x.FullPath);
+                    if (_assetTypeService.FileExtensions.Contains(extension)) {
+                        fileObserver.OnNext(x);
+                    }
+                }
+            })
+            .DisposeWith(_disposables);
 
-        return _assetTypeService.FileExtensions.Contains(extension);
+        bool Filter(T t) {
+            // Ignore changes in the delete directory
+            if (t.FullPath.StartsWith(_deleteDirectoryProvider.DeleteDirectory, AssetCompare.PathComparison)) return false;
+
+            return true;
+        }
     }
 
     public void Dispose() => _disposables.Dispose();
--- CreationEditor/Services/Asset/IDataDirectoryService.cs
+++ CreationEditor/Services/Asset/IDataDirectoryService.cs
@@ -7,22 +7,54 @@ public interface IDataDirectoryService : IDisposable {
     string Path { get; }
 
     /// <summary>
-    /// Emits the event args of a newly created file.
+    /// Emits the event args of a newly created file or directory.
     /// </summary>
     IObservable<FileSystemEventArgs> Created { get; }
+    /// <summary>
+    /// Emits the event args of a newly created file.
+    /// </summary>
+    IObservable<FileSystemEventArgs> CreatedFile { get; }
+    /// <summary>
+    /// Emits the event args of a newly created directory.
+    /// </summary>
+    IObservable<FileSystemEventArgs> CreatedDirectory { get; }
 
+    /// <summary>
+    /// Emits the event args of a deleted file or directory.
+    /// </summary>
+    IObservable<FileSystemEventArgs> Deleted { get; }
     /// <summary>
     /// Emits the event args of a deleted file.
     /// </summary>
-    IObservable<FileSystemEventArgs> Deleted { get; }
+    IObservable<FileSystemEventArgs> DeletedFile { get; }
+    /// <summary>
+    /// Emits the event args of a deleted directory.
+    /// </summary>
+    IObservable<FileSystemEventArgs> DeletedDirectory { get; }
 
+    /// <summary>
+    /// Emits the event args of a changed file or directory.
+    /// </summary>
+    IObservable<FileSystemEventArgs> Changed { get; }
     /// <summary>
     /// Emits the event args of a changed file.
     /// </summary>
-    IObservable<FileSystemEventArgs> Changed { get; }
+    IObservable<FileSystemEventArgs> ChangedFile { get; }
+    /// <summary>
+    /// Emits the event args of a changed directory.
+    /// </summary>
+    IObservable<FileSystemEventArgs> ChangedDirectory { get; }
 
+    /// <summary>
+    /// Emits the event args of a renamed file or directory.
+    /// </summary>
+    IObservable<RenamedEventArgs> Renamed { get; }
     /// <summary>
     /// Emits the event args of a renamed file.
     /// </summary>
-    IObservable<RenamedEventArgs> Renamed { get; }
+    IObservable<RenamedEventArgs> RenamedFile { get; }
+    /// <summary>
+    /// Emits the event args of a renamed directory.
+    /// </summary>
+    IObservable<RenamedEventArgs> RenamedDirectory { get; }
 }
