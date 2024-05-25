c96898bfc8f2e0c348485aec40542efbe4feb037
Elscrux <nickposer2102@gmail.com>
2024-05-25 19:27:35 +0200

    Improve mod selection view styling

--- CreationEditor.Avalonia/Views/Mod/ModSelectionDialog.axaml.cs
+++ CreationEditor.Avalonia/Views/Mod/ModSelectionDialog.axaml.cs
@@ -1,10 +1,26 @@
-﻿using Avalonia.ReactiveUI;
+﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia;
+using Avalonia.ReactiveUI;
 using CreationEditor.Avalonia.ViewModels.Mod;
+using Noggog;
+using ReactiveUI;
 namespace CreationEditor.Avalonia.Views.Mod;
 
 public partial class ModSelectionPopup : ReactiveUserControl<ModSelectionVM> {
     public ModSelectionPopup() {
         InitializeComponent();
+
+        this.WhenActivated(disposables => {
+            this.WhenAnyValue(x => x.ViewModel)
+                .NotNull()
+                .Subscribe(vm => {
+                    LoadButton[!ContentProperty] = vm.AnyModsActive
+                        .Select(anyModActive => anyModActive ? "Load" : "Create")
+                        .ToBinding();
+                })
+                .DisposeWith(disposables);
+        });
     }
 
     public ModSelectionPopup(ModSelectionVM modSelectionVM) : this() {
