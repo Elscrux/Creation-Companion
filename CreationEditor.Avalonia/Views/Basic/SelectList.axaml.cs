62aed2de44b5aab6899f31499d516fb31079f799
Elscrux <nickposer2102@gmail.com>
2025-05-29 20:02:46 +0200

    fix: selectable list menu items only working on second try

--- CreationEditor.Avalonia/Views/Basic/SelectList.axaml.cs
+++ CreationEditor.Avalonia/Views/Basic/SelectList.axaml.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Primitives;
 using Avalonia.Controls.Templates;
 using Avalonia.Metadata;
+using CreationEditor.Avalonia.Command;
 namespace CreationEditor.Avalonia.Views.Basic;
 
+[TemplatePart(Name = ItemsControlName, Type = typeof(ItemsControl))]
 public class SelectList : TemplatedControl {
+    private const string ItemsControlName = "PART_ItemsControl";
+
     public static readonly StyledProperty<IEnumerable> ItemsSourceProperty
         = AvaloniaProperty.Register<SelectList, IEnumerable>(nameof(ItemsSource));
 
@@ -21,5 +27,32 @@ public class SelectList : TemplatedControl {
     public IDataTemplate DataTemplate {
         get => GetValue(DataTemplateProperty);
         set => SetValue(DataTemplateProperty, value);
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e) {
+        base.OnApplyTemplate(e);
+
+        var itemsControl = e.NameScope.Find<ItemsControl>(ItemsControlName);
+        if (itemsControl is null) return;
+
+        itemsControl.ContextFlyout ??= new MenuFlyout {
+            ItemsSource = (MenuItem[]) [
+                new MenuItem {
+                    Command = SelectableCommand.SelectAll,
+                    CommandParameter = ItemsSource,
+                    Header = "Select All",
+                },
+                new MenuItem {
+                    Command = SelectableCommand.UnselectAll,
+                    CommandParameter = ItemsSource,
+                    Header = "Unselect All",
+                },
+                new MenuItem {
+                    Command = SelectableCommand.ToggleAll,
+                    CommandParameter = ItemsSource,
+                    Header = "Toggle All",
+                },
+            ]
+        };
     }
 }
