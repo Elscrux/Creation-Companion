9f80bc3fee78b2485b4f683e43ebb174aa7a6798
Elscrux <nickposer2102@gmail.com>
2025-05-29 20:59:27 +0200

    fix: crash when enumerable can't be cast - cast individual items instead

--- CreationEditor.Avalonia/Resources/Command/SelectableCommand.cs
+++ CreationEditor.Avalonia/Resources/Command/SelectableCommand.cs
@@ -1,32 +1,33 @@
-﻿using System.Reactive;
+﻿using System.Collections;
+using System.Reactive;
 using CreationEditor.Avalonia.Models.Selectables;
 using ReactiveUI;
 namespace CreationEditor.Avalonia.Command;
 
 public static class SelectableCommand {
-    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>?, Unit> SelectAll =
-        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>?>(selectables => {
+    public static readonly ReactiveCommand<IEnumerable?, Unit> SelectAll =
+        ReactiveCommand.Create<IEnumerable?>(selectables => {
             if (selectables is null) return;
 
-            foreach (var selectable in selectables) {
+            foreach (var selectable in selectables.Cast<IReactiveSelectable>()) {
                 selectable.IsSelected = true;
             }
         });
 
-    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>?, Unit> UnselectAll =
-        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>?>(selectables => {
+    public static readonly ReactiveCommand<IEnumerable?, Unit> UnselectAll =
+        ReactiveCommand.Create<IEnumerable?>(selectables => {
             if (selectables is null) return;
 
-            foreach (var selectable in selectables) {
+            foreach (var selectable in selectables.Cast<IReactiveSelectable>()) {
                 selectable.IsSelected = false;
             }
         });
 
-    public static readonly ReactiveCommand<IEnumerable<IReactiveSelectable>?, Unit> ToggleAll =
-        ReactiveCommand.Create<IEnumerable<IReactiveSelectable>?>(selectables => {
+    public static readonly ReactiveCommand<IEnumerable?, Unit> ToggleAll =
+        ReactiveCommand.Create<IEnumerable?>(selectables => {
             if (selectables is null) return;
 
-            foreach (var selectable in selectables) {
+            foreach (var selectable in selectables.Cast<IReactiveSelectable>()) {
                 selectable.IsSelected = !selectable.IsSelected;
             }
         });
