aa2b1a2f2a57f1bfac32e61e438a86eef9f311c3
Elscrux <nickposer2102@gmail.com>
2025-06-01 01:22:00 +0200

    chore: remove old comments

--- CreationEditor/Services/DataSource/IDataSource.cs
+++ CreationEditor/Services/DataSource/IDataSource.cs
@@ -3,9 +3,6 @@ using CreationEditor.Core;
 using Mutagen.Bethesda.Assets;
 namespace CreationEditor.Services.DataSource;
 
-// TODO analyze asset browser startup and see what makes it so slow
-// TODO cut any unneeded initialization that is not necessary - try lazy or so otherwise
-
 /// <summary>
 /// Represents a data source that acts as a data folder for the game.
 /// </summary>
@@ -13,7 +10,7 @@ public interface IDataSource : IMementoProvider<DataSourceMemento> {
     string Name { get; }
     string Path { get; }
     bool IsReadOnly { get; }
-    // todo replace all use of file system anywhere with this
+
     IFileSystem FileSystem { get; }
 
     string GetFullPath(DataRelativePath path);
