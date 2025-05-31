674e01b4203f5d6d320870281927a0e1b99b04b4
Elscrux <nickposer2102@gmail.com>
2025-05-31 19:05:55 +0200

    feat: add GetBrush method

--- CreationEditor.Avalonia/Resources/Constants/StandardBrushes.cs
+++ CreationEditor.Avalonia/Resources/Constants/StandardBrushes.cs
@@ -4,20 +4,20 @@ using Avalonia.Media;
 namespace CreationEditor.Avalonia.Constants;
 
 public static class StandardBrushes {
-    public static IBrush? HighlightBrush =>
-        Application.Current is not null
-     && Application.Current.TryFindResource("SystemAccentColor", Application.Current.ActualThemeVariant, out var obj)
-     && obj is Color color
-            ? new SolidColorBrush(color)
-            : null;
+    public static IBrush? HighlightBrush => GetBrush("SystemAccentColor");
 
-    public static IBrush? BackgroundBrush =>
-        Application.Current is not null
-     && Application.Current.TryFindResource("SolidBackgroundFillColorTertiary", Application.Current.ActualThemeVariant, out var obj)
-     && obj is Color color
-            ? new SolidColorBrush(color)
-            : null;
+    public static IBrush? BackgroundBrush => GetBrush("SolidBackgroundFillColorTertiary");
 
     public static ISolidColorBrush ValidBrush => Brushes.ForestGreen;
     public static ISolidColorBrush InvalidBrush => Brushes.IndianRed;
+
+    public static SolidColorBrush? GetBrush(string dynamicColorKey) =>
+        Application.Current is not null
+     && Application.Current.TryFindResource(dynamicColorKey, Application.Current.ActualThemeVariant, out var obj)
+            ? obj switch {
+                Color color => new SolidColorBrush(color),
+                SolidColorBrush brush => brush,
+                _ => null
+            }
+            : null;
 }
