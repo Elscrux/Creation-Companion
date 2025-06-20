7c0878bca3e8dcd45220e667b16b13cefb1636ac
Elscrux <nickposer2102@gmail.com>
2025-06-20 19:31:20 +0200

    feat: add asset promotion to promote to master plugin

--- Plugins/PromoteToMaster/ViewModels/PromoteToMasterVM.cs
+++ Plugins/PromoteToMaster/ViewModels/PromoteToMasterVM.cs
@@ -4,22 +4,31 @@ using Avalonia.Threading;
 using CreationEditor;
 using CreationEditor.Avalonia.ViewModels;
 using CreationEditor.Avalonia.ViewModels.Mod;
+using CreationEditor.Services.Asset;
+using CreationEditor.Services.DataSource;
 using CreationEditor.Services.Environment;
 using CreationEditor.Services.Mutagen.Record;
+using CreationEditor.Services.Mutagen.References.Asset.Controller;
 using CreationEditor.Services.Mutagen.References.Record;
 using CreationEditor.Services.Mutagen.References.Record.Controller;
+using CreationEditor.Skyrim;
 using CreationEditor.Skyrim.Definitions;
 using DynamicData.Binding;
 using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Assets;
 using Mutagen.Bethesda.Plugins.Records;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
 namespace PromoteToMaster.ViewModels;
 
-// TODO Add asset handling
 public sealed partial class PromoteToMasterVM : ViewModel {
+    public static readonly IReadOnlyList<AssetPromotionMode> AssetPromotionModes = Enum.GetValues<AssetPromotionMode>();
+
     private readonly IEditorEnvironment<ISkyrimMod, ISkyrimModGetter> _editorEnvironment;
+    private readonly IAssetReferenceController _assetReferenceController;
+    private readonly IAssetController _assetController;
 
     public IReadOnlyList<IReferencedRecord> RecordsToPromote { get; }
 
@@ -30,6 +39,7 @@ public sealed partial class PromoteToMasterVM : ViewModel {
     public SingleModPickerVM EditMod { get; }
 
     public IObservableCollection<RecordPromotionChange> RecordPromotionChanges { get; } = new ObservableCollectionExtended<RecordPromotionChange>();
+    public IObservableCollection<AssetPromotionChange> AssetPromotionChanges { get; } = new ObservableCollectionExtended<AssetPromotionChange>();
 
     [Reactive] public partial string? RemovePrefix { get; set; }
     [Reactive] public partial string? AddPrefix { get; set; }
@@ -39,15 +49,25 @@ public sealed partial class PromoteToMasterVM : ViewModel {
     public ReactiveCommand<Unit, Unit> SettingsConfirmed { get; }
     public ReactiveCommand<Unit, Unit> Run { get; }
 
+    public AssetPromotionMode AssetPromotionMode { get; set; } = AssetPromotionMode.ForceMove;
+    public IObservable<bool> NoAssetTarget { get; }
+    public IObservableCollection<IDataSource> AssetOrigins { get; } = new ObservableCollectionExtended<IDataSource>();
+    public IObservableCollection<IDataSource> AssetTargets { get; } = new ObservableCollectionExtended<IDataSource>();
+
     public PromoteToMasterVM(
         IEditorEnvironment<ISkyrimMod, ISkyrimModGetter> editorEnvironment,
+        IDataSourceService dataSourceService,
         IRecordReferenceController recordReferenceController,
         IRecordController recordController,
+        IAssetReferenceController assetReferenceController,
+        IAssetController assetController,
         SingleModPickerVM injectToMod,
         SingleModPickerVM injectedRecordCreationMod,
         SingleModPickerVM editMod,
         IReadOnlyList<IReferencedRecord> recordsToPromote) {
         _editorEnvironment = editorEnvironment;
+        _assetReferenceController = assetReferenceController;
+        _assetController = assetController;
         RecordsToPromote = recordsToPromote;
         RecordReferenceController = recordReferenceController;
         RecordController = recordController;
@@ -64,16 +84,38 @@ public sealed partial class PromoteToMasterVM : ViewModel {
         EditMod.CanCreateNewMod = true;
         EditMod.Filter = MutableModsFilter;
 
+        dataSourceService
+            .DataSourcesChanged
+            .Subscribe(dataSources => {
+                dataSources = dataSources.Where(FilterDataSources).ToList();
+                AssetOrigins.Load(dataSources.Except(AssetTargets));
+                AssetTargets.Load(dataSources.Intersect(AssetTargets));
+            })
+            .DisposeWith(this);
+
         var allModsSelected = InjectToMod.HasModSelected.CombineLatest(
             InjectedRecordCreationMod.HasModSelected,
             EditMod.HasModSelected,
             (a, b, c) => a && b && c);
         SettingsConfirmed = ReactiveCommand.CreateRunInBackground(() => {
-                // Only run this once
-                if (RecordPromotionChanges.Count > 0) return;
+                if (InjectToMod.SelectedMod is null) return;
 
-                var recordPromotionChanges = GetAffectedRecords(RecordsToPromote).ToList();
-                Dispatcher.UIThread.Post(() => RecordPromotionChanges.Load(recordPromotionChanges));
+                // Only run this once
+                IReadOnlyList<RecordPromotionChange> recordPromotionChanges;
+                if (RecordPromotionChanges.Count == 0) {
+                    var injectionTarget = _editorEnvironment.GetMod(InjectToMod.SelectedMod.ModKey);
+                    recordPromotionChanges = GetAffectedRecords(RecordsToPromote, injectionTarget).ToList();
+                    Dispatcher.UIThread.Post(() => RecordPromotionChanges.Load(recordPromotionChanges));
+                } else {
+                    recordPromotionChanges = RecordPromotionChanges.ToList();
+                }
+
+                var promotedRecords = recordPromotionChanges
+                    .Where(x => x.ChangeType == RecordPromotionChangeType.Deleted)
+                    .Select(x => x.Record)
+                    .ToArray();
+                var assetPromotionChanges = GetAffectedAssets(promotedRecords).ToList();
+                Dispatcher.UIThread.Post(() => AssetPromotionChanges.Load(assetPromotionChanges));
             },
             allModsSelected
         );
@@ -84,11 +126,28 @@ public sealed partial class PromoteToMasterVM : ViewModel {
                     : record => CalculateEditorID(record.EditorID, RemovePrefix, AddPrefix));
             },
             allModsSelected);
+
+        var assetTargetCountChanges = this.WhenAnyValue(x => x.AssetTargets.Count);
+
+        NoAssetTarget = assetTargetCountChanges.Select(count => count == 0);
+
+        // Make sure there is only at max one target data source
+        assetTargetCountChanges
+            .Where(count => count > 1)
+            .Subscribe(_ => {
+                AssetOrigins.AddRange(AssetTargets.Skip(1).ToArray());
+                AssetTargets.RemoveRange(1, AssetTargets.Count - 1);
+            })
+            .DisposeWith(this);
+    }
+
+    private bool FilterDataSources(IDataSource dataSource) {
+        return !dataSource.IsReadOnly;
     }
 
     private bool MutableModsFilter(IModKeyed mod) {
         // Vanilla masters shouldn't be edited
-        if (SkyrimDefinitions.SkyrimModKeys.Contains(mod.ModKey)) return false;
+        if (MemoryExtensions.Contains(SkyrimDefinitions.SkyrimModKeys, mod.ModKey)) return false;
 
         // The mod should be mutable
         if (_editorEnvironment.LinkCache.GetMod(mod.ModKey) is not IMod) return false;
@@ -101,7 +160,7 @@ public sealed partial class PromoteToMasterVM : ViewModel {
 
     private bool FilterInjectToMod(IModKeyed mod) {
         // Vanilla masters are not valid injection targets 
-        if (SkyrimDefinitions.SkyrimModKeys.Contains(mod.ModKey)) return false;
+        if (MemoryExtensions.Contains(SkyrimDefinitions.SkyrimModKeys, mod.ModKey)) return false;
 
         // The injection target allows all records to be promoted to it
         return RecordsToPromote.Select(x => x.FormKey.ModKey)
@@ -135,34 +194,132 @@ public sealed partial class PromoteToMasterVM : ViewModel {
             formKey => recordReferenceDictionary[formKey].RecordReferences,
             editorIdMapper,
             ForceDelete);
+
+        if (AssetTargets is not [var assetTarget]) return;
+        foreach (var promotion in AssetPromotionChanges) {
+            var targetLink = new FileSystemLink(assetTarget, promotion.FileSystemLink.DataRelativePath);
+            
+            switch (promotion.ChangeType) {
+                case AssetPromotionChangeType.Moved:
+                    _assetController.Move(promotion.FileSystemLink, targetLink);
+                    break;
+                case AssetPromotionChangeType.Copied:
+                    _assetController.Copy(promotion.FileSystemLink, targetLink);
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
     }
 
-    public IEnumerable<RecordPromotionChange> GetAffectedRecords(IReadOnlyCollection<IReferencedRecord> recordsToBePromoted) {
-        var includedRecords = new HashSet<FormKey>(recordsToBePromoted.Select(x => x.FormKey));
+    public IEnumerable<RecordPromotionChange> GetAffectedRecords(
+        IReadOnlyCollection<IReferencedRecord> recordsToBePromoted,
+        ISkyrimModGetter injectionTarget) {
+        var allMastersAndInjection = injectionTarget.GetTransitiveMasters(_editorEnvironment.GameEnvironment, true).ToArray();
+
+        // Gather all linked records that are not already in the target mod
+        var includedRecords = new HashSet<IMajorRecordGetter>(recordsToBePromoted.Select(x => x.Record));
+        var recordsToCheck = new Queue<IMajorRecordGetter>();
+        recordsToCheck.Enqueue(recordsToBePromoted.Select(x => x.Record));
+        while (recordsToCheck.Count > 0) {
+            var record = recordsToCheck.Dequeue();
+
+            // Enqueue all linked records to check them as well
+            foreach (var link in record.EnumerateFormLinks()) {
+                if (allMastersAndInjection.Contains(link.FormKey.ModKey)) continue;
+                if (!_editorEnvironment.LinkCache.TryResolve(link, out var linkedRecord)) continue;
+
+                if (includedRecords.Add(linkedRecord)) {
+                    recordsToCheck.Enqueue(linkedRecord);
+                }
+            }
+        }
 
         // Promoted records will be deleted
-        foreach (var referencedRecord in recordsToBePromoted) {
-            yield return new RecordPromotionChange(referencedRecord.Record, PromotionChangeType.Deleted);
+        foreach (var record in includedRecords) {
+            yield return new RecordPromotionChange(record, RecordPromotionChangeType.Deleted);
         }
 
         // References will relink to the promoted records
+        var includedFormKeys = includedRecords.Select(x => x.FormKey).ToArray();
         foreach (var record in recordsToBePromoted) {
             var references = RecordReferenceController.GetReferences(record.FormKey).ToArray();
             if (references.Length == 0) continue;
 
             foreach (var reference in references) {
-                if (includedRecords.Contains(reference.FormKey)) continue;
+                if (includedFormKeys.Contains(reference.FormKey)) continue;
                 if (!_editorEnvironment.LinkCache.TryResolveContext(reference, out var context)) continue;
 
-                yield return new RecordPromotionChange(context.Record, PromotionChangeType.Modified);
+                yield return new RecordPromotionChange(context.Record, RecordPromotionChangeType.Modified);
+            }
+        }
+    }
+
+    public IEnumerable<AssetPromotionChange> GetAffectedAssets(IReadOnlyCollection<IMajorRecordGetter> records) {
+        if (AssetPromotionMode == AssetPromotionMode.Nothing) yield break;
+        if (AssetTargets.Count != 1) yield break;
+
+        var recordIdentifiers = records.Select(r => r.ToFormLinkInformation()).ToArray();
+        var allReferencedAssets = records
+            .SelectMany(r => r.EnumerateAssetLinks(AssetLinkQuery.Listed | AssetLinkQuery.Inferred))
+            .ToArray();
+
+        var allReferencedDataRelativePaths = allReferencedAssets
+            .Select(assetLink => assetLink.DataRelativePath)
+            .ToHashSet();
+
+        foreach (var assetLink in allReferencedAssets) {
+            var dataSource = AssetOrigins.FirstOrDefault(dataSource => dataSource.FileExists(assetLink.DataRelativePath));
+            if (dataSource is null) continue;
+
+            var fileSystemLink = new FileSystemLink(dataSource, assetLink.DataRelativePath);
+
+            switch (AssetPromotionMode) {
+                case AssetPromotionMode.Copy:
+                    yield return new AssetPromotionChange(fileSystemLink, AssetPromotionChangeType.Copied);
+
+                    break;
+                case AssetPromotionMode.Move:
+                    var allAssetRefsIncluded = _assetReferenceController
+                        .GetAssetReferences(assetLink)
+                        .All(path => allReferencedDataRelativePaths.Contains(path));
+
+                    var allRecordRefsIncluded = _assetReferenceController
+                        .GetRecordReferences(assetLink)
+                        .All(recordIdentifiers.Contains);
+
+                    if (allAssetRefsIncluded && allRecordRefsIncluded) {
+                        yield return new AssetPromotionChange(fileSystemLink, AssetPromotionChangeType.Moved);
+                    } else {
+                        yield return new AssetPromotionChange(fileSystemLink, AssetPromotionChangeType.Copied);
+                    }
+
+                    break;
+                case AssetPromotionMode.ForceMove:
+                    yield return new AssetPromotionChange(fileSystemLink, AssetPromotionChangeType.Moved);
+
+                    break;
             }
         }
     }
 }
 
-public sealed record RecordPromotionChange(IMajorRecordGetter Record, PromotionChangeType ChangeType);
+public sealed record RecordPromotionChange(IMajorRecordGetter Record, RecordPromotionChangeType ChangeType);
+public sealed record AssetPromotionChange(FileSystemLink FileSystemLink, AssetPromotionChangeType ChangeType);
 
-public enum PromotionChangeType {
+public enum RecordPromotionChangeType {
     Modified,
     Deleted,
+}
+
+public enum AssetPromotionChangeType {
+    Moved,
+    Copied,
+}
+
+public enum AssetPromotionMode {
+    Nothing,
+    Copy,
+    Move,
+    ForceMove,
 }
--- Plugins/PromoteToMaster/Views/PromoteToMasterView.axaml.cs
+++ Plugins/PromoteToMaster/Views/PromoteToMasterView.axaml.cs
@@ -1,8 +1,20 @@
-﻿using Avalonia.ReactiveUI;
+﻿using Avalonia.Data.Converters;
+using Avalonia.ReactiveUI;
 using PromoteToMaster.ViewModels;
 namespace PromoteToMaster.Views;
 
 public partial class PromoteToMasterView : ReactiveUserControl<PromoteToMasterVM> {
+    public static readonly FuncValueConverter<AssetPromotionMode, string> AssetPromotionModeToString
+        = new(mode =>
+            mode switch {
+                AssetPromotionMode.Nothing => "Nothing will be moved",
+                AssetPromotionMode.Copy => "All assets referenced by the records will be copied over to the 'To' data source",
+                AssetPromotionMode.Move => "The assets referenced by the records will be moved over in case they are not referenced by anything in the 'From' data sources anymore, otherwise they will be copied",
+                AssetPromotionMode.ForceMove => "All assets referenced by the records will be moved over regardless if they are still referenced by something in the 'From' data sources",
+                _ => string.Empty,
+            }
+        );
+
     public PromoteToMasterView() {
         InitializeComponent();
     }
