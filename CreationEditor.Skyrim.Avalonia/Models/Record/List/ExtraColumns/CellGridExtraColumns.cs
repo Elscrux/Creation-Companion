ff3e3b46894a18fcbfebf9121af28942aff79dcf
Elscrux <nickposer2102@gmail.com>
2025-06-30 13:25:16 +0200

    chore: misc extra column tweaks

--- CreationEditor.Skyrim.Avalonia/Models/Record/List/ExtraColumns/CellGridExtraColumns.cs
+++ CreationEditor.Skyrim.Avalonia/Models/Record/List/ExtraColumns/CellGridExtraColumns.cs
@@ -1,15 +1,17 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Data;
 using CreationEditor.Avalonia.Models.Record.List.ExtraColumns;
+using CreationEditor.Services.Mutagen.References.Record;
 using CreationEditor.Skyrim.Avalonia.Resources.Comparer;
+using Mutagen.Bethesda.Skyrim;
 namespace CreationEditor.Skyrim.Avalonia.Models.Record.List.ExtraColumns;
 
-public class CellGridExtraColumns : IUntypedExtraColumns {
+public sealed class CellGridExtraColumns : IUntypedExtraColumns {
     public IEnumerable<ExtraColumn> CreateColumns() {
         yield return new ExtraColumn(
             new DataGridTextColumn {
                 Header = "Grid",
-                Binding = new Binding("Record.Grid.Point", BindingMode.OneWay),
+                Binding = new Binding(nameof(IReferencedRecord.Record) + '.' + nameof(ICellGetter.Grid) + '.'  +  nameof(ICellGetter.Grid.Point), BindingMode.OneWay),
                 CanUserSort = true,
                 CustomSortComparer = SkyrimRecordComparers.CellGridComparer,
                 Width = new DataGridLength(100),
