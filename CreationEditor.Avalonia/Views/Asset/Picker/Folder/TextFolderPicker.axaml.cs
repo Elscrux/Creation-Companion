78cf6c2d4b4d315fc105aafc18e50ce4f0c07015
Elscrux <nickposer2102@gmail.com>
2025-07-02 16:14:29 +0200

    feat: add TextFolderPicker

---
+++ CreationEditor.Avalonia/Views/Asset/Picker/Folder/TextFolderPicker.axaml.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using CreationEditor.Avalonia.Views.Asset.Picker.File;
+namespace CreationEditor.Avalonia.Views.Asset.Picker.Folder;
+
+public class TextFolderPicker : AFolderPicker {
+    public static readonly StyledProperty<string?> WatermarkProperty
+        = AvaloniaProperty.Register<TextFilePicker, string?>(nameof(Watermark));
+
+    public string? Watermark {
+        get => GetValue(WatermarkProperty);
+        set => SetValue(WatermarkProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> AllowTextEditProperty
+        = AvaloniaProperty.Register<TextFilePicker, bool>(nameof(AllowTextEdit));
+
+    public bool AllowTextEdit {
+        get => GetValue(AllowTextEditProperty);
+        set => SetValue(AllowTextEditProperty, value);
+    }
+}
