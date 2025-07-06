aeccaac314894b1d21fafcef9e5973aa0332728e
Elscrux <nickposer2102@gmail.com>
2025-07-06 13:29:43 +0200

    feat: add menu item to open file that defines list

--- Plugins/LeveledList/Views/ListsView.axaml.cs
+++ Plugins/LeveledList/Views/ListsView.axaml.cs
@@ -1,5 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using System.Diagnostics;
+using System.Reactive.Disposables;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
+using LeveledList.Model.List;
 using LeveledList.ViewModels;
 
 namespace LeveledList.Views;
@@ -12,5 +16,14 @@ public partial class ListsView : ReactiveUserControl<ListsVM> {
     private void Control_OnContextRequested(object? sender, ContextRequestedEventArgs e) {
         ViewModel?.ContextMenu(sender, e);
     }
-}
 
+    private void OpenFile(object? sender, RoutedEventArgs e) {
+        if (ListsDataGrid.SelectedItem is not ExtendedListDefinition listDefinition) return;
+
+        Process.Start(new ProcessStartInfo {
+            FileName = listDefinition.Path,
+            UseShellExecute = true,
+            Verb = "open",
+        });
+    }
+}
