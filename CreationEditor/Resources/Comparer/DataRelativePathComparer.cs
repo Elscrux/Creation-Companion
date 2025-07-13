29c94d594737fd36b82430ec7b05af117e3296f4
Elscrux <nickposer2102@gmail.com>
2025-07-13 15:59:05 +0200

    feat: add DataRelativePathComparer

---
+++ CreationEditor/Resources/Comparer/DataRelativePathComparer.cs
@@ -0,0 +1,9 @@
+﻿using Mutagen.Bethesda.Assets;
+using Noggog;
+namespace CreationEditor.Resources.Comparer;
+
+public class DataRelativePathComparer() : FuncEqualityComparer<DataRelativePath>(
+    (a, b) => DataRelativePath.PathComparer.Equals(a, b),
+    a => DataRelativePath.PathComparer.GetHashCode(a)) {
+    public static DataRelativePathComparer Instance { get; } = new();
+}
