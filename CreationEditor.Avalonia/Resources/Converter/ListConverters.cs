f47a556379304c7cc6c38259935d02689ceee7b2
Elscrux <nickposer2102@gmail.com>
2025-07-17 00:03:41 +0200

    feat: add JoinToString converter

--- CreationEditor.Avalonia/Resources/Converter/ListConverters.cs
+++ CreationEditor.Avalonia/Resources/Converter/ListConverters.cs
@@ -1,7 +1,18 @@
 ﻿using System.Collections;
+using Noggog;
 namespace CreationEditor.Avalonia.Converter;
 
 public static class ListConverters {
     public static readonly ExtendedFuncValueConverter<IList, bool, object> Contains
         = new((list, e) => list is not null && list.Contains(e));
+
+    public static readonly ExtendedFuncValueConverter<IEnumerable, string, object> JoinToString
+        = new((enumerable, e) => {
+            if (enumerable is null || e is not string s) return string.Empty;
+
+            return string.Join(s, enumerable
+                .OfType<object?>()
+                .WhereNotNull()
+                .Select(e => e.ToString()));
+        });
 }
