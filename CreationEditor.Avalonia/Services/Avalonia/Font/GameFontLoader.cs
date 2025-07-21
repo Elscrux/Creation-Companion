29b179664d2f9d73058c895a49a21e189a25e947
Elscrux <nickposer2102@gmail.com>
2025-07-21 17:51:07 +0200

    fix: create fonts directory if it doesn't exist

--- CreationEditor.Avalonia/Services/Avalonia/Font/GameFontLoader.cs
+++ CreationEditor.Avalonia/Services/Avalonia/Font/GameFontLoader.cs
@@ -1,12 +1,17 @@
-﻿using Avalonia.Media;
+﻿using System.IO.Abstractions;
+using Avalonia.Media;
 using Mutagen.Bethesda.Environments.DI;
 namespace CreationEditor.Avalonia.Services.Avalonia.Font;
 
 public class GameFontLoader : IGameFontLoader {
+    private readonly IFileSystem _fileSystem;
     private readonly Uri _fontsDirectoryUri = new(new Uri(AppContext.BaseDirectory), "./Resources/Fonts");
     private readonly string _fontCollectionName;
 
-    public GameFontLoader(IGameReleaseContext gameReleaseContext) {
+    public GameFontLoader(
+        IFileSystem fileSystem,
+        IGameReleaseContext gameReleaseContext) {
+        _fileSystem = fileSystem;
         _fontCollectionName = $"fonts:{gameReleaseContext.Release}";
 
         Init();
@@ -14,6 +19,12 @@ public class GameFontLoader : IGameFontLoader {
 
     private void Init() {
         // todo automatically extract ttf fonts from swf files as referenced in (fontProvider.FontLibraries) and copy them into ./Resources/Fonts
+
+        // Create directory
+        if (!_fileSystem.Directory.Exists(_fontsDirectoryUri.LocalPath)) {
+            _fileSystem.Directory.CreateDirectory(_fontsDirectoryUri.LocalPath);
+        }
+
         FontManager.Current.AddFontCollection(new FileSystemFontCollection(new Uri(_fontCollectionName), _fontsDirectoryUri));
     }
 
