fb6cd2f8fd2b41c397a5eaee1653cf927bbd61f9
Elscrux <nickposer2102@gmail.com>
2025-08-06 01:15:04 +0200

    feat: expand common aspects

--- CreationEditor.Skyrim.Avalonia/Services/Types/SkyrimCommonAspectsProvider.cs
+++ CreationEditor.Skyrim.Avalonia/Services/Types/SkyrimCommonAspectsProvider.cs
@@ -1,15 +1,52 @@
 ﻿using CreationEditor.Services.Mutagen.Type;
 using Mutagen.Bethesda.Plugins.Records;
 using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Strings;
 namespace CreationEditor.Skyrim.Avalonia.Services.Types;
 
 public sealed class SkyrimCommonAspectsProvider : IMutagenCommonAspectsProvider {
     public Type AddonNodeRecordType { get; } = typeof(IAddonNodeGetter);
     public Type SoundDescriptorRecordType { get; } = typeof(ISoundDescriptorGetter);
+    public Type BookType { get; } = typeof(IBookGetter);
+    public Type MessageType { get; } = typeof(IMessageGetter);
+    public Type DialogTopic { get; } = typeof(IDialogTopicGetter);
+    public Type DialogResponses { get; } = typeof(IDialogResponsesGetter);
 
     public int? GetAddonNodeIndex(IMajorRecordGetter record) {
         if (record is IAddonNodeGetter addonNode) {
             return addonNode.NodeIndex;
+        }
+
+        return null;
+    }
+
+    public ITranslatedStringGetter? GetBookText(IMajorRecordGetter record) {
+        if (record is IBookGetter book) {
+            return book.BookText;
+        }
+
+        return null;
+    }
+
+    public ITranslatedStringGetter? GetMessageDescription(IMajorRecordGetter record) {
+        if (record is IMessageGetter message) {
+            return message.Description;
+        }
+
+        return null;
+    }
+
+    public ITranslatedStringGetter? GetDialogTopicName(IMajorRecordGetter record) {
+        if (record is IDialogTopicGetter dialogTopic) {
+            return dialogTopic.Name;
+        }
+
+        return null;
+    }
+
+    public ITranslatedStringGetter? GetDialogResponsesPrompt(IMajorRecordGetter record) {
+        if (record is IDialogResponsesGetter dialogResponses) {
+            return dialogResponses.Prompt;
         }
 
         return null;
--- CreationEditor/Services/Mutagen/Type/IMutagenCommonAspectsProvider.cs
+++ CreationEditor/Services/Mutagen/Type/IMutagenCommonAspectsProvider.cs
@@ -1,9 +1,18 @@
 ﻿using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Strings;
 namespace CreationEditor.Services.Mutagen.Type;
 
 public interface IMutagenCommonAspectsProvider {
     System.Type AddonNodeRecordType { get; }
     System.Type SoundDescriptorRecordType { get; }
+    System.Type BookType { get; }
+    System.Type MessageType { get; }
+    System.Type DialogTopic { get; }
+    System.Type DialogResponses { get; }
 
     int? GetAddonNodeIndex(IMajorRecordGetter record);
+    ITranslatedStringGetter? GetBookText(IMajorRecordGetter record);
+    ITranslatedStringGetter? GetMessageDescription(IMajorRecordGetter record);
+    ITranslatedStringGetter? GetDialogTopicName(IMajorRecordGetter record);
+    ITranslatedStringGetter? GetDialogResponsesPrompt(IMajorRecordGetter record);
 }
