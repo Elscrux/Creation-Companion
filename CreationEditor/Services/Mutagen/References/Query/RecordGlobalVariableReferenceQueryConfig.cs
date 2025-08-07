1d11efcc6328b7a316c98bd4846a6b37a3edbb3a
Elscrux <nickposer2102@gmail.com>
2025-08-08 00:31:09 +0200

    fix: place global variable reference cache in references folder

--- CreationEditor/Services/Mutagen/References/Query/RecordGlobalVariableReferenceQueryConfig.cs
+++ CreationEditor/Services/Mutagen/References/Query/RecordGlobalVariableReferenceQueryConfig.cs
@@ -7,13 +7,16 @@ namespace CreationEditor.Services.Mutagen.References.Query;
 public sealed class RecordGlobalVariableReferenceQueryConfig(
     ReferenceCacheBuilder referenceCacheBuilder,
     RecordGlobalVariableReferenceQuery globalVariableReferenceQuery,
-    DictionaryReferenceCacheSerialization<IModGetter, DictionaryReferenceCache<string, IFormLinkIdentifier>, string, IFormLinkIdentifier> serialization)
+    Func<IReadOnlyList<string>, DictionaryReferenceCacheSerialization<IModGetter, DictionaryReferenceCache<string, IFormLinkIdentifier>, string, IFormLinkIdentifier>> serializationFactory)
     : IReferenceQueryConfig<IModGetter, RecordModPair, DictionaryReferenceCache<string, IFormLinkIdentifier>, string> {
+    private readonly DictionaryReferenceCacheSerialization<IModGetter, DictionaryReferenceCache<string, IFormLinkIdentifier>, string, IFormLinkIdentifier> _serialization =
+        serializationFactory(["References"]);
+
     public bool CanGetLinksFromDeletedElement => true;
     public string Name => globalVariableReferenceQuery.Name;
 
     public Task<DictionaryReferenceCache<string, IFormLinkIdentifier>> BuildCache(IModGetter source) {
-        return referenceCacheBuilder.BuildCache(source, globalVariableReferenceQuery, serialization);
+        return referenceCacheBuilder.BuildCache(source, globalVariableReferenceQuery, _serialization);
     }
 
     public IEnumerable<string> GetLinks(RecordModPair element) {
