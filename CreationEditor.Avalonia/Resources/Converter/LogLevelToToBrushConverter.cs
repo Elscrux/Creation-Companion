aae3186d74e3fe12c20c70d848c81f6b564e29be
Elscrux <nickposer2102@gmail.com>
2025-09-14 10:55:08 +0200

    refactor: add option to  bind log text brush

--- CreationEditor.Avalonia/Resources/Converter/LogLevelToToBrushConverter.cs
+++ CreationEditor.Avalonia/Resources/Converter/LogLevelToToBrushConverter.cs
@@ -1,16 +1,52 @@
 ﻿using System.Globalization;
+using Avalonia;
 using Avalonia.Data.Converters;
 using Avalonia.Media;
 using Serilog.Events;
 namespace CreationEditor.Avalonia.Converter;
 
-public sealed class LogLevelToToBrushConverter : IValueConverter {
-    public ISolidColorBrush? VerboseBrush { get; set; }
-    public ISolidColorBrush? DebugBrush { get; set; }
-    public ISolidColorBrush? MessageBrush { get; set; }
-    public ISolidColorBrush? WarningBrush { get; set; }
-    public ISolidColorBrush? ErrorBrush { get; set; }
-    public ISolidColorBrush? FatalBrush { get; set; }
+public sealed class LogLevelToToBrushConverter : AvaloniaObject, IValueConverter {
+    public static readonly StyledProperty<IBrush?> VerboseBrushProperty =
+        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(VerboseBrush));
+    public IBrush? VerboseBrush {
+        get => GetValue(VerboseBrushProperty);
+        set => SetValue(VerboseBrushProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush?> DebugBrushProperty =
+        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(DebugBrush));
+    public IBrush? DebugBrush {
+        get => GetValue(DebugBrushProperty);
+        set => SetValue(DebugBrushProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush?> MessageBrushProperty
+        = AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(MessageBrush));
+    public IBrush? MessageBrush {
+        get => GetValue(MessageBrushProperty);
+        set => SetValue(MessageBrushProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush?> WarningBrushProperty =
+        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(WarningBrush));
+    public IBrush? WarningBrush {
+        get => GetValue(WarningBrushProperty);
+        set => SetValue(WarningBrushProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush?> ErrorBrushProperty =
+        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(ErrorBrush));
+    public IBrush? ErrorBrush {
+        get => GetValue(ErrorBrushProperty);
+        set => SetValue(ErrorBrushProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush?> FatalBrushProperty =
+        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(FatalBrush));
+    public IBrush? FatalBrush {
+        get => GetValue(FatalBrushProperty);
+        set => SetValue(FatalBrushProperty, value);
+    }
 
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
         if (!EnumExtension.TryParse<LogEventLevel>(value, out var level)) return null;
