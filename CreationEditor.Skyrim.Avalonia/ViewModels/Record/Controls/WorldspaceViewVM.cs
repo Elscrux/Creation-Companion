83ab25de179835a2236205a332fa68e9463ba4e6
Elscrux <nickposer2102@gmail.com>
2025-10-05 18:03:10 +0200

    feat: add worldspace view

---
+++ CreationEditor.Skyrim.Avalonia/ViewModels/Record/Controls/WorldspaceViewVM.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Media;
+using Avalonia.Threading;
+using CreationEditor.Avalonia.ViewModels;
+using CreationEditor.Skyrim.Avalonia.Services.Map;
+using Mutagen.Bethesda.Skyrim;
+using ReactiveUI.SourceGenerators;
+namespace CreationEditor.Skyrim.Avalonia.ViewModels.Record.Controls;
+
+public sealed partial class WorldspaceViewVM : ViewModel {
+    public IWorldspaceGetter Worldspace { get; }
+
+    [Reactive] public partial IImage? DrawingsImage { get; set; } = null!;
+    [Reactive] public partial bool IsBusy { get; set; }
+
+    public WorldspaceViewVM(
+        RegionMapCreator regionMapCreator,
+        IWorldspaceGetter worldspace,
+        Func<ICellGetter, Color>? cellColorProvider = null) {
+        Worldspace = worldspace;
+        IsBusy = true;
+
+        Task.Run(async () => {
+            var regionDrawing = await regionMapCreator.GetRegionMap(worldspace, cellColorProvider: cellColorProvider);
+            Dispatcher.UIThread.Post(() => {
+                DrawingsImage = new DrawingImage(regionDrawing);
+                IsBusy = false;
+            });
+        });
+    }
+}
---
+++ CreationEditor.Skyrim.Avalonia/Views/Record/Controls/WorldspaceView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.ReactiveUI;
+using WorldspaceViewVM = CreationEditor.Skyrim.Avalonia.ViewModels.Record.Controls.WorldspaceViewVM;
+namespace CreationEditor.Skyrim.Avalonia.Views.Record.Controls;
+
+public partial class WorldspaceView : ReactiveUserControl<WorldspaceViewVM> {
+    public WorldspaceView() {
+        InitializeComponent();
+    }
+
+    public WorldspaceView(WorldspaceViewVM vm) : this() {
+        DataContext = vm;
+    }
+}
