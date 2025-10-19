be8ea81265de95f6709644dbb55a596e6d55adf7
Elscrux <nickposer2102@gmail.com>
2025-10-19 18:34:01 +0200

    feat: add csv as standard file type

--- CreationEditor.Avalonia/Resources/Constants/StandardFileTypes.cs
+++ CreationEditor.Avalonia/Resources/Constants/StandardFileTypes.cs
@@ -3,4 +3,11 @@ namespace CreationEditor.Avalonia.Constants;
 
 public static class StandardFileTypes {
     public static readonly IReadOnlyList<FilePickerFileType> ImageAll = [FilePickerFileTypes.ImageAll];
+    public static readonly IReadOnlyList<FilePickerFileType> Csv = [
+        new("Csv file") {
+            Patterns = ["*.csv"],
+            AppleUniformTypeIdentifiers = ["public.comma-separated-values-text"],
+            MimeTypes = ["text/csv"]
+        }
+    ];
 }
