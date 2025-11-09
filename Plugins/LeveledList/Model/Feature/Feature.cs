bba965ef890447d8d543491853088e13a4943a5a
Elscrux <nickposer2102@gmail.com>
2025-11-09 16:59:01 +0100

    feat(leveled-list): make debugger info readable

--- Plugins/LeveledList/Model/Feature/Feature.cs
+++ Plugins/LeveledList/Model/Feature/Feature.cs
@@ -1,3 +1,7 @@
 ﻿namespace LeveledList.Model.Feature;
 
-public record Feature(FeatureWildcard Wildcard, object Key);
+public record Feature(FeatureWildcard Wildcard, object Key) {
+    public override string ToString() {
+        return $"{Wildcard.Identifier}={Key}";
+    }
+};
--- Plugins/LeveledList/Model/Feature/FeatureNode.cs
+++ Plugins/LeveledList/Model/Feature/FeatureNode.cs
@@ -1,6 +1,11 @@
-﻿using LeveledList.Model.Tier;
+﻿using System.Diagnostics;
+using LeveledList.Model.Tier;
 namespace LeveledList.Model.Feature;
 
+[DebuggerDisplay("{ToString()}}")]
 public record FeatureNode(List<Feature> Features, IReadOnlyList<RecordWithTier> Records) {
     public List<FeatureNode> Children { get; } = [];
+    public override string ToString() {
+        return $"FeatureNode({Features.LastOrDefault()}, Records={Records.Count}, Children={Children.Count})";
+    }
 }
