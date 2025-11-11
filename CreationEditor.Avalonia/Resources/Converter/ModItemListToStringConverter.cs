4800a4a4b39dab950eb5e97bb6a4989c2a6854d2
Elscrux <nickposer2102@gmail.com>
2025-11-11 22:31:57 +0100

    feat: add ModPickerButton variant of ModPicker

---
+++ CreationEditor.Avalonia/Resources/Converter/ModItemListToStringConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+using Mutagen.Bethesda.Plugins;
+namespace CreationEditor.Avalonia.Converter;
+
+public sealed class ModItemListToStringConverter : IValueConverter {
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+        if (value is not IEnumerable<IModKeyed> modKeys) return null;
+
+        var convert = string.Join(", ", modKeys.Select(x => x.ModKey.FileName));
+        if (string.IsNullOrEmpty(convert)) return "None";
+
+        return convert;
+
+    }
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => throw new NotImplementedException();
+}
---
+++ CreationEditor.Avalonia/Views/Mod/ModPickerButton.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.ReactiveUI;
+using CreationEditor.Avalonia.ViewModels.Mod;
+namespace CreationEditor.Avalonia.Views.Mod;
+
+public partial class ModPickerButton : ReactiveUserControl<IModPickerVM> {
+    public ModPickerButton() {
+        InitializeComponent();
+    }
+
+    public ModPickerButton(IModPickerVM vm) : this() {
+        DataContext = vm;
+    }
+}
