340a7df6a76d522526e1b278c7952da4ec07d21e
Elscrux <nickposer2102@gmail.com>
2026-01-24 22:34:51 +0100

    fix: double To in name of LogLevelToBrushConverter

--- CreationEditor.Avalonia/Resources/Converter/LogLevelToBrushConverter.cs
+++ CreationEditor.Avalonia/Resources/Converter/LogLevelToBrushConverter.cs
@@ -5,44 +5,44 @@ using Avalonia.Media;
 using Serilog.Events;
 namespace CreationEditor.Avalonia.Converter;
 
-public sealed class LogLevelToToBrushConverter : AvaloniaObject, IValueConverter {
+public sealed class LogLevelToBrushConverter : AvaloniaObject, IValueConverter {
     public static readonly StyledProperty<IBrush?> VerboseBrushProperty =
-        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(VerboseBrush));
+        AvaloniaProperty.Register<LogLevelToBrushConverter, IBrush?>(nameof(VerboseBrush));
     public IBrush? VerboseBrush {
         get => GetValue(VerboseBrushProperty);
         set => SetValue(VerboseBrushProperty, value);
     }
 
     public static readonly StyledProperty<IBrush?> DebugBrushProperty =
-        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(DebugBrush));
+        AvaloniaProperty.Register<LogLevelToBrushConverter, IBrush?>(nameof(DebugBrush));
     public IBrush? DebugBrush {
         get => GetValue(DebugBrushProperty);
         set => SetValue(DebugBrushProperty, value);
     }
 
     public static readonly StyledProperty<IBrush?> MessageBrushProperty
-        = AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(MessageBrush));
+        = AvaloniaProperty.Register<LogLevelToBrushConverter, IBrush?>(nameof(MessageBrush));
     public IBrush? MessageBrush {
         get => GetValue(MessageBrushProperty);
         set => SetValue(MessageBrushProperty, value);
     }
 
     public static readonly StyledProperty<IBrush?> WarningBrushProperty =
-        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(WarningBrush));
+        AvaloniaProperty.Register<LogLevelToBrushConverter, IBrush?>(nameof(WarningBrush));
     public IBrush? WarningBrush {
         get => GetValue(WarningBrushProperty);
         set => SetValue(WarningBrushProperty, value);
     }
 
     public static readonly StyledProperty<IBrush?> ErrorBrushProperty =
-        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(ErrorBrush));
+        AvaloniaProperty.Register<LogLevelToBrushConverter, IBrush?>(nameof(ErrorBrush));
     public IBrush? ErrorBrush {
         get => GetValue(ErrorBrushProperty);
         set => SetValue(ErrorBrushProperty, value);
     }
 
     public static readonly StyledProperty<IBrush?> FatalBrushProperty =
-        AvaloniaProperty.Register<LogLevelToToBrushConverter, IBrush?>(nameof(FatalBrush));
+        AvaloniaProperty.Register<LogLevelToBrushConverter, IBrush?>(nameof(FatalBrush));
     public IBrush? FatalBrush {
         get => GetValue(FatalBrushProperty);
         set => SetValue(FatalBrushProperty, value);
