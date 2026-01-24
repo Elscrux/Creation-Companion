4ec2ca6e31082a5e15a2fcb6d237cb6be84c2248
Elscrux <nickposer2102@gmail.com>
2026-01-24 23:19:12 +0100

    feat: add FuncValueConverter extension functions to call them directly

--- CreationEditor.Avalonia/Extension/ConverterExtension.cs
+++ CreationEditor.Avalonia/Extension/ConverterExtension.cs
@@ -6,17 +6,20 @@ public static class ConverterExtension {
     private static readonly Type PlaceholderType = typeof(string);
 
     extension(IValueConverter converter) {
-        public object? Convert(object? value, object? parameter) {
-            return converter.Convert(value, PlaceholderType, parameter, CultureInfo.InvariantCulture);
-        }
-        public object? Convert(object? value) {
-            return converter.Convert(value, null);
-        }
-        public object? ConvertBack(object? value, object? parameter) {
-            return converter.ConvertBack(value, PlaceholderType, parameter, CultureInfo.InvariantCulture);
-        }
-        public object? ConvertBack(object? value) {
-            return converter.ConvertBack(value, null);
-        }
+        public object? Convert(object? value, object? parameter) => converter.Convert(value, PlaceholderType, parameter, CultureInfo.InvariantCulture);
+        public object? Convert(object? value) => converter.Convert(value, null);
+        public object? ConvertBack(object? value, object? parameter) => converter.ConvertBack(value, PlaceholderType, parameter, CultureInfo.InvariantCulture);
+        public object? ConvertBack(object? value) => converter.ConvertBack(value, null);
+    }
+
+    extension<TIn, TOut>(FuncValueConverter<TIn, TOut> converter) {
+        public TOut? Convert(TIn? value, object? parameter) => converter.Convert(value, PlaceholderType, parameter, CultureInfo.InvariantCulture) is TOut x
+            ? x
+            : default;
+        public TOut? Convert(TIn? value) => converter.Convert(value, null);
+        public TIn? ConvertBack(TOut? value, object? parameter) => converter.ConvertBack(value, PlaceholderType, parameter, CultureInfo.InvariantCulture) is TIn x
+            ? x
+            : default;
+        public TIn? ConvertBack(TOut? value) => converter.ConvertBack(value, null);
     }
 }
