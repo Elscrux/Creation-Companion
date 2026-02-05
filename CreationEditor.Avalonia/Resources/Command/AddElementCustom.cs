15ffcad30d32d86416b412642765c7a8fda02376
Elscrux <nickposer2102@gmail.com>
2026-02-05 20:19:22 +0100

    feat: add AddElementCustom to be able to specify a constructor when adding an element

---
+++ CreationEditor.Avalonia/Resources/Command/AddElementCustom.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+namespace CreationEditor.Avalonia.Command;
+
+public sealed class AddElementCustom<T> : ListCommand<T> {
+    public static readonly StyledProperty<Func<T>> FactoryProperty
+        = AvaloniaProperty.Register<AvaloniaObject, Func<T>>(nameof(List));
+
+    public Func<T> Factory {
+        get => GetValue(FactoryProperty);
+        set => SetValue(FactoryProperty, value);
+    }
+
+    public override void Execute(object? parameter) {
+        List?.Add(Factory.Invoke());
+    }
+}
