e221e7e5039d2d5749885605eb25a92f37d2fa45
Elscrux <nickposer2102@gmail.com>
2026-02-10 20:46:12 +0100

    feat: add regex replacement view

---
+++ CreationEditor.Avalonia/ViewModels/Utility/RegexReplacementVM.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using ReactiveUI.SourceGenerators;
+namespace CreationEditor.Avalonia.ViewModels.Utility;
+
+/// <summary>
+/// Represents a pair of regex pattern and replacement.
+/// </summary>
+public sealed partial class RegexReplacementVM : ViewModel {
+    [Reactive] public partial string Pattern { get; set; } = string.Empty;
+    [Reactive] public partial string Replacement { get; set; } = string.Empty;
+
+    public RegexReplacementVM() {}
+
+    public RegexReplacementVM(string pattern, string replacement) {
+        Pattern = pattern;
+        Replacement = replacement;
+    }
+
+    public string Replace(string name) {
+        if (string.IsNullOrWhiteSpace(Pattern)) return name;
+
+        return Regex.Replace(name, Pattern, Replacement);
+    }
+}
---
+++ CreationEditor.Avalonia/Views/Utility/RegexReplacement.axaml.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.ReactiveUI;
+using CreationEditor.Avalonia.ViewModels.Utility;
+namespace CreationEditor.Avalonia.Views.Utility;
+
+public partial class RegexReplacement : ReactiveUserControl<RegexReplacementVM> {
+    public RegexReplacement() {
+        InitializeComponent();
+    }
+}
