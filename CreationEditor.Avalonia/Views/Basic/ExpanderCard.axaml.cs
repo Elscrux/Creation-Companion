97f2802153abd40dfb005d104a9503647a31a229
Elscrux <nickposer2102@gmail.com>
2026-02-11 13:36:25 +0100

    refactor: update expander card to be extending Expander

--- CreationEditor.Avalonia/Views/Basic/ExpanderCard.axaml.cs
+++ CreationEditor.Avalonia/Views/Basic/ExpanderCard.axaml.cs
@@ -1,3 +1,30 @@
-﻿namespace CreationEditor.Avalonia.Views.Basic;
+﻿using Avalonia;
+using Avalonia.Controls;
+using FluentAvalonia.UI.Controls;
+namespace CreationEditor.Avalonia.Views.Basic;
 
-public class ExpanderCard : IconHeaderedContentControl;
+public class ExpanderCard : Expander {
+    public static readonly StyledProperty<Symbol> IconProperty
+        = AvaloniaProperty.Register<ExpanderCard, Symbol>(nameof(Icon));
+
+    public Symbol Icon {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+
+    public static readonly StyledProperty<Control?> IconControlProperty
+        = AvaloniaProperty.Register<ExpanderCard, Control?>(nameof(IconControl));
+
+    public Control? IconControl {
+        get => GetValue(IconControlProperty);
+        set => SetValue(IconControlProperty, value);
+    }
+
+    public static readonly StyledProperty<string?> DescriptionProperty
+        = AvaloniaProperty.Register<ExpanderCard, string?>(nameof(Description));
+
+    public string? Description {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+}
