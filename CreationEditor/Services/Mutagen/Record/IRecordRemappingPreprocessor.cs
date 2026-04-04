6a5cf803b0dea08a77b305b7e680493d17a10e9d
Elscrux <nickposer2102@gmail.com>
2026-04-04 14:20:41 +0200

    feat: add record remapping preprocessor interface which is meant to

---
+++ CreationEditor/Services/Mutagen/Record/IRecordRemappingPreprocessor.cs
@@ -0,0 +1,13 @@
+﻿using CreationEditor.Services.Mutagen.References;
+using Mutagen.Bethesda.Plugins;
+namespace CreationEditor.Services.Mutagen.Record;
+
+/// <summary>
+/// Preprocessors which will all be run before a record is remapped if applicable
+/// </summary>
+public interface IRecordRemappingPreprocessor {
+    string Description { get; }
+
+    bool IsApplicable(IReferencedRecord referencedRecord);
+    void PreprocessRemapping(IReferencedRecord record, FormKey remappingFormKey);
+}
