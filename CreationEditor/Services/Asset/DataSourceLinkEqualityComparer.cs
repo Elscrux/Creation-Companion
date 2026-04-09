e63a2c32f634a90f85745cb57345d137ea35152a
Elscrux <nickposer2102@gmail.com>
2026-04-09 02:05:59 +0200

    feat: add DataSourceLinkEqualityComparer

---
+++ CreationEditor/Services/Asset/DataSourceLinkEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using CreationEditor.Services.DataSource;
+namespace CreationEditor.Services.Asset;
+
+public sealed class DataSourceLinkEqualityComparer : IEqualityComparer<IDataSourceLink> {
+    public static readonly DataSourceLinkEqualityComparer Instance = new();
+
+    public bool Equals(IDataSourceLink? x, IDataSourceLink? y) {
+        if (ReferenceEquals(x, y)) return true;
+        if (x is null || y is null) return false;
+
+        return x.DataSource.Equals(y.DataSource) && x.DataRelativePath.Equals(y.DataRelativePath);
+    }
+
+    public int GetHashCode(IDataSourceLink obj) {
+        return HashCode.Combine(obj.DataSource, obj.DataRelativePath);
+    }
+}
