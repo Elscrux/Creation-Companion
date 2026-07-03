6f3e54d8a599b2ac39ecc8f7c43a936c09928d75
Elscrux <nickposer2102@gmail.com>
2026-07-03 13:37:32 +0200

    feat(save): disable remove identical to master records to false by default because it can take a long time on big files

--- CreationEditor.Avalonia/Models/Settings/Save/SaveSettings.cs
+++ CreationEditor.Avalonia/Models/Settings/Save/SaveSettings.cs
@@ -9,5 +9,5 @@ public sealed partial class SaveSettings : ReactiveObject, ISettingModel {
     [JsonProperty, Reactive] public partial SaveLocation SaveLocation { get; set; }
     [JsonProperty, Reactive] public partial string DataRelativeOrFullCustomSaveLocation { get; set; } = string.Empty;
 
-    [JsonProperty, Reactive] public partial bool RemoveIdenticalToMasterRecords { get; set; } = true;
+    [JsonProperty, Reactive] public partial bool RemoveIdenticalToMasterRecords { get; set; } = false;
 }
