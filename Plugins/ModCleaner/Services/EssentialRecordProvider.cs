8ea2dbec37fde32912064b058b1e5c41372cefe1
Elscrux <nickposer2102@gmail.com>
2026-07-06 15:50:38 +0200

    refactor(mod-cleaner): move FeatureFlagExtensions functions into IEssentialRecordProvider

--- Plugins/ModCleaner/Services/EssentialRecordProvider.cs
+++ Plugins/ModCleaner/Services/EssentialRecordProvider.cs
@@ -1,9 +1,13 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using CreationEditor.Services.Environment;
+using CreationEditor.Skyrim;
 using ModCleaner.Services.FeatureFlag;
+using Mutagen.Bethesda;
 using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Cache;
 using Mutagen.Bethesda.Plugins.Records;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
 namespace ModCleaner.Services;
 
 public sealed class EssentialRecordProvider(
@@ -24,7 +28,7 @@ public sealed class EssentialRecordProvider(
         .ToHashSet();
 
     private IEnumerable<FormLinkInformation> EnumerateEssentialRecords() {
-        foreach (var (worldspace, cells) in featureFlagService.EnabledFeatureFlags.EnumerateRetainedCells(editorEnvironment.LinkCache)) {
+        foreach (var (worldspace, cells) in EnumerateRetainedExteriorCells(editorEnvironment.LinkCache)) {
             yield return new FormLinkInformation(worldspace, typeof(IWorldspaceGetter));
 
             foreach (var cell in cells) {
@@ -40,11 +44,39 @@ public sealed class EssentialRecordProvider(
     }
 
     public bool IsInvalidExteriorCell(IFormLinkGetter<IWorldspaceGetter> worldspace, ICellGetter cell) {
-        var allowedRegions = featureFlagService.EnabledFeatureFlags.GetAllowedRegions(worldspace);
+        var allowedRegions = GetAllowedRegions(worldspace);
 
         // When there is no reference of the worldspace in any feature flag, all cells are valid
         if (allowedRegions.Count == 0) return false;
 
         return cell.Regions is null || !cell.Regions.Intersect(allowedRegions).Any();
+    }
+
+    public Dictionary<FormKey, List<ICellGetter>> EnumerateRetainedExteriorCells(ILinkCache linkCache) {
+        var mergedFeatureFlags = featureFlagService.EnabledFeatureFlags
+            .SelectMany(x => x.AllowedRegions)
+            .GroupBy(x => x.Worldspace);
+
+        var retainedCells = new Dictionary<FormKey, List<ICellGetter>>();
+        foreach (var group in mergedFeatureFlags) {
+            if (!group.Key.TryResolve(linkCache, out var worldspace)) continue;
+
+            var regions = group.SelectMany(y => y.Regions).ToHashSet();
+            var cells = regions.Count == 0
+                ? worldspace.EnumerateCells()
+                : worldspace.EnumerateCells().Where(c => c.Regions is not null && c.Regions.Intersect(regions).Any());
+
+            foreach (var cell in cells) {
+                retainedCells.GetOrAdd(worldspace.FormKey).Add(cell);
+            }
+        }
+
+        return retainedCells;
+    }
+
+    public IReadOnlyList<IFormLinkGetter<IRegionGetter>> GetAllowedRegions(IFormLinkGetter<IWorldspaceGetter> worldspace) {
+        return featureFlagService.EnabledFeatureFlags
+            .SelectMany(f => f.AllowedRegions.Find(ar => ar.Worldspace.Equals(worldspace))?.Regions ?? [])
+            .ToArray();
     }
 }
