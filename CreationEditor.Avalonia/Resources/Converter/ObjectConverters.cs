1fa4a53eb543db0f324b6da9d72ac7f3c4092f67
Elscrux <nickposer2102@gmail.com>
2026-08-31 21:59:07 +0200

    style(build-stripper): only enable feature flags that are matching the selected mod

--- CreationEditor.Avalonia/Resources/Converter/ObjectConverters.cs
+++ CreationEditor.Avalonia/Resources/Converter/ObjectConverters.cs
@@ -1,6 +1,15 @@
-﻿namespace CreationEditor.Avalonia.Converter;
+﻿using Avalonia.Data.Converters;
+namespace CreationEditor.Avalonia.Converter;
 
 public static class ObjectConverters {
     public new static readonly ExtendedFuncValueConverter<object, bool, object> Equals
         = new((obj, parameter) => Equals(obj, parameter));
+
+    public static readonly FuncMultiValueConverter<object, bool> EqualsTwoBindings
+        = new(objs => {
+            if (objs is not [var a, var b]) return false;
+            if (a is null) return b is null;
+
+            return a.Equals(b);
+        });
 }
